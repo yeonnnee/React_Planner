6cda7ff549c2ec3d80f78bc0f1067079222c927d
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-09 00:56:19 +0900

    PERSIST saved data at the tasks.json file

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -3,35 +3,42 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-const tasks = [];
-
 const fileLocation = path.join(
   path.dirname(process.mainModule.filename),
   "data",
   "tasks.json"
 );
 
-router.get("/tasks", (req, res) => {
-  const data = fs.readFileSync(fileLocation);
-  const tasks = data.toString();
-  res.json(tasks);
-});
+function getData() {
+  const loadData = fs.readFileSync(fileLocation);
+  const data = loadData.toString();
+  return data;
+}
 
-router.post("/tasks", (req, res) => {
-  console.log(req.body);
-  const text = req.body.tasks.text;
-  const id = req.body.tasks.id;
-
-  tasks.push({ text: text, id: id });
-  const content = JSON.stringify(tasks);
-  console.log(tasks);
+function saveData(tasks, res) {
+  const content = JSON.stringify(tasks, null, 2);
   fs.writeFile(fileLocation, content, function (err) {
     if (err) {
       console.log(err);
     } else {
-      res.send({ tasks: { text: text, id: id } });
+      res.send("has successfully received");
     }
   });
+}
+
+router.get("/tasks", (req, res) => {
+  const data = getData();
+  res.json(data);
+});
+
+router.post("/tasks", (req, res) => {
+  const data = getData();
+  const tasks = JSON.parse(data);
+  const text = req.body.tasks.text;
+  const id = req.body.tasks.id;
+
+  tasks.push({ text: text, id: id });
+  saveData(tasks, res);
 });
 
 module.exports = router;
