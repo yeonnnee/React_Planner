9926653950993dd02219f501b714034c5e9f8bee
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-11 23:52:38 +0900

    CREATE router

---
+++ src/components/router.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/display-name */
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Tasks from "./Tasks";
+
+export default () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Tasks} />
+      </Switch>
+    </Router>
+  );
+};
