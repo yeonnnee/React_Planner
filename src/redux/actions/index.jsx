20781e66c9bab5a0c77e5f4dbafa185ddd88bce0
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-11 13:43:16 +0900

    CREATE actions

--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -1,3 +1,6 @@
+export const FETCH_START = "[TASKS] fetching data start";
 export const FETCH_SUCCESS = "[TASKS] fetching data success";
 export const FETCH_FAILED = "[TASKS] fetching data failed";
+
 export const ADD_TASKS = "[TASKS] add new tasks";
+export const DELETE_TASKS = "[TASKS] delete tasks";
