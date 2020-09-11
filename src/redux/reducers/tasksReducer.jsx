5b8c8e14380d205455a1098e7ca5ebef147e52d5
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-11 23:52:03 +0900

    SET actions for change tasks status

--- src/redux/reducers/tasksReducer.jsx
+++ src/redux/reducers/tasksReducer.jsx
@@ -3,38 +3,66 @@ import {
   FETCH_FAILED,
   FETCH_START,
   ADD_TASKS,
+  FINISHED_TASKS,
 } from "../actions";
 
 export const initialState = {
   isLoading: true,
   error: "",
-  tasks: [],
+  pendingList: [],
+  finishedList: [],
 };
 
 const tasksReducer = (state = initialState, action) => {
   switch (action.type) {
+    //////////////////////* FETCH TASKS DATA REDUCER *////////////////
     case FETCH_START:
       return {
         ...state,
         isLoading: true,
       };
-    case FETCH_SUCCESS:
+    case FETCH_SUCCESS: {
+      const filtered_PendingList = action.payload.filter(
+        (task) => task.status === "PENDING"
+      );
+      const filtered_FinishedList = action.payload.filter(
+        (task) => task.status === "FINISHED"
+      );
       return {
         ...state,
         isLoading: false,
-        tasks: [...state.tasks, ...action.payload],
+        pendingList: [...state.pendingList, ...filtered_PendingList],
+        finishedList: [...state.finishedList, ...filtered_FinishedList],
       };
+    }
     case FETCH_FAILED:
       return {
         ...state,
         isLoading: false,
         error: "Something went wrong",
       };
+    //////////////////////* TASKS REDUCER *////////////////
     case ADD_TASKS:
       return {
         ...state,
-        tasks: [...state.tasks, action.payload],
+        isLoading: false,
+        pendingList: [...state.pendingList, action.payload],
       };
+    case FINISHED_TASKS: {
+      let completed = state.pendingList.find(
+        (task) => task.id === action.payload.id
+      );
+      completed = { ...completed, status: "FINISHED" };
+      const filtered_pendingList = state.pendingList.filter(
+        (task) => task.id !== action.payload.id
+      );
+      return {
+        ...state,
+        isLoading: false,
+        pendingList: [{ ...filtered_pendingList }],
+        finishedList: [{ ...state.finishedList, completed }],
+      };
+    }
     default:
       return state;
   }
