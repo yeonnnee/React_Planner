e702c1d28c6e60054a124ef66ae63b297c0492ac
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-12 21:49:06 +0900

    SET fonts

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 
 import App from "./components/App";
 import store from "./redux/store";
+import "./components/styles/fonts.css";
 
 ReactDOM.render(
   <React.StrictMode>
