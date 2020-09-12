cb51dbba8a5f4261030fab39281ee8cdb6c8ebdd
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-12 14:51:01 +0900

    CREATE change status action

--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -4,4 +4,5 @@ export const FETCH_FAILED = "[TASKS] fetching data failed";
 
 export const ADD_TASKS = "[TASKS] add new tasks";
 export const DELETE_TASKS = "[TASKS] delete tasks";
-export const FINISHED_TASKS = "[TASKS] finished tasks";
+export const CHANGE_STATUS =
+  "[TASKS] change task status to FINISHED or PENDING";
