1a2b2294f311bc546a8c84fb9d9453bdd1d98ef5
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-12 21:49:27 +0900

    STYLED taskList

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,6 +3,37 @@ import React from "react";
 import { connect } from "react-redux";
 import { CHANGE_STATUS, DELETE_TASKS } from "../redux/actions";
 import axios from "axios";
+import styled from "styled-components";
+
+const List = styled.li`
+  padding: 5px 20px;
+  display: grid;
+  grid-template-columns: 20px 1fr 20px;
+`;
+const CheckBtn = styled.input`
+  display: none;
+`;
+const Label = styled.label`
+  width: 15px;
+  height: 15px;
+  border: 1px solid #5a2330;
+  opacity: 0.6;
+  &:hover {
+    opacity: 1;
+    border: 1px solid #5a2330;
+  }
+`;
+const DelBtn = styled.button`
+  outline: none;
+  border: none;
+  background-color: transparent;
+  opacity: 0.6;
+  cursor: pointer;
+  &:hover {
+    opacity: 1;
+    color: #5a2330;
+  }
+`;
 
 const TaskList = ({ text, id, status, change, deleteItem }) => {
   function changeStatus() {
@@ -25,11 +56,17 @@ const TaskList = ({ text, id, status, change, deleteItem }) => {
     deleteItem(target);
   }
   return (
-    <div>
-      <input type="checkbox" onClick={changeStatus} defaultChecked={false} />
-      <li>{text}</li>
-      <button onClick={deleteTask}>x</button>
-    </div>
+    <List>
+      <CheckBtn
+        type="checkbox"
+        onClick={changeStatus}
+        defaultChecked={false}
+        id={id}
+      />
+      <Label htmlFor={id}></Label>
+      {text}
+      <DelBtn onClick={deleteTask}>x</DelBtn>
+    </List>
   );
 };
 
