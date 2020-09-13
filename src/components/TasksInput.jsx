d3048c836552ecb9fc23b8c8d636457ef790595a
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-13 23:29:55 +0900

    Trim code and fix bug which error message has been shown once appeared

--- src/components/TasksInput.jsx
+++ src/components/TasksInput.jsx
@@ -4,10 +4,9 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { ADD_TASKS } from "../redux/actions";
+import ErrorMessage from "./ErrorMessage";
 
 const Form = styled.form`
   display: flex;
@@ -19,23 +18,11 @@ const Input = styled.input`
   width: 320px;
   padding: 10px 20px;
   border-radius: 20px;
-  /* box-shadow: 5px 5px 5px #ad8d92, -5px -5px 15px #e3d8d4,
-    inset -5px -5px 5px #e3d8d4; */
   box-shadow: 5px 5px 5px #cfc0bd, inset -3px -3px 5px #e3d8d4,
     inset 1px 1px 3px #e3d8d4;
   &::placeholder {
     font-family: "Zilla Slab", serif;
   }
-`;
-const ErrorMessage = styled.div`
-  margin-top: 8px;
-  margin-left: 80px;
-  font-size: 12px;
-  color: #90323d;
-`;
-
-const SFontAwesomeIcon = styled(FontAwesomeIcon)`
-  margin-right: 5px;
 `;
 
 const TasksInput = ({ add }) => {
@@ -51,6 +38,10 @@ const TasksInput = ({ add }) => {
     const value = event.target.value;
     setState({
       tasks: { text: value, id: uuidv4().toString(), status: "PENDING" },
+    });
+    setError({
+      error: "",
+      message: "",
     });
   }
   async function onSubmit(event) {
@@ -73,14 +64,9 @@ const TasksInput = ({ add }) => {
           value={state.tasks.text}
           onChange={onChange}
           placeholder="Add a Task..."
-        ></Input>
+        />
       </Form>
-      {error.message !== "" ? (
-        <ErrorMessage>
-          <SFontAwesomeIcon icon={faExclamationCircle} />
-          {error.message}
-        </ErrorMessage>
-      ) : null}
+      {error.message !== "" ? <ErrorMessage {...error} /> : null}
     </>
   );
 };
