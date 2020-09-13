77a5decac1f1bd55f4091cfa91f6ec406e84b6c2
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-13 22:07:26 +0900

    CREATE error message for input validation

--- src/components/TasksInput.jsx
+++ src/components/TasksInput.jsx
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
 import { connect } from "react-redux";
 import styled from "styled-components";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { ADD_TASKS } from "../redux/actions";
 
@@ -17,17 +19,32 @@ const Input = styled.input`
   width: 320px;
   padding: 10px 20px;
   border-radius: 20px;
-  box-shadow: 5px 5px 5px #ad8d92, -5px -5px 15px #e3d8d4,
-    inset -5px -5px 5px #e3d8d4;
-
+  /* box-shadow: 5px 5px 5px #ad8d92, -5px -5px 15px #e3d8d4,
+    inset -5px -5px 5px #e3d8d4; */
+  box-shadow: 5px 5px 5px #cfc0bd, inset -3px -3px 5px #e3d8d4,
+    inset 1px 1px 3px #e3d8d4;
   &::placeholder {
     font-family: "Zilla Slab", serif;
   }
+`;
+const ErrorMessage = styled.div`
+  margin-top: 8px;
+  margin-left: 80px;
+  font-size: 12px;
+  color: #90323d;
+`;
+
+const SFontAwesomeIcon = styled(FontAwesomeIcon)`
+  margin-right: 5px;
 `;
 
 const TasksInput = ({ add }) => {
   const [state, setState] = useState({
     tasks: { text: "", id: "", status: "" },
+  });
+  const [error, setError] = useState({
+    error: "",
+    message: "",
   });
 
   function onChange(event) {
@@ -36,18 +53,35 @@ const TasksInput = ({ add }) => {
       tasks: { text: value, id: uuidv4().toString(), status: "PENDING" },
     });
   }
-  function onSubmit(event) {
+  async function onSubmit(event) {
     event.preventDefault();
     if (state.tasks.text !== "") {
-      axios.post("/api/tasks", state);
-      add(state.tasks);
+      const res = await axios.post("/api/tasks", state);
+      const result = res.data;
+      if (result !== "Get data successfully") {
+        return setError({ error: result.error, message: result.msg });
+      }
     }
-    setState({ tasks: { text: "", id: "" } });
+    add(state.tasks);
+    setState({ tasks: { text: "", id: "", status: "" } });
   }
   return (
-    <Form onSubmit={onSubmit}>
-      <Input type="text" value={state.tasks.text} onChange={onChange}></Input>
-    </Form>
+    <>
+      <Form onSubmit={onSubmit}>
+        <Input
+          type="text"
+          value={state.tasks.text}
+          onChange={onChange}
+          placeholder="Add a Task..."
+        ></Input>
+      </Form>
+      {error.message !== "" ? (
+        <ErrorMessage>
+          <SFontAwesomeIcon icon={faExclamationCircle} />
+          {error.message}
+        </ErrorMessage>
+      ) : null}
+    </>
   );
 };
 
