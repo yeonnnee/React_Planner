90fa99f5e6c0ecd6ef755c286253699a34f902a5
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-14 19:03:25 +0900

    CREATE sign up router

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Tasks from "../container/Tasks";
 import Home from "../container/Home";
 import LogIn from "../container/LogIn";
+import SignUp from "../container/SignUp";
 
 import Header from "./Header";
 
@@ -15,6 +16,7 @@ export default () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/logIn" component={LogIn} />
+        <Route path="/sign-up" component={SignUp} />
         <Route path="/tasks" component={Tasks} />
       </Switch>
     </Router>
