89c393527d0b8356d6f3f4226020408d668c9cd0
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-15 18:51:59 +0900

    REVEISE user model

--- servers/models/user.js
+++ servers/models/user.js
@@ -13,52 +13,19 @@ const User = sequelize.define(
     name: {
       type: Sequelize.STRING(10),
       allowNull: false,
-      validate: {
-        isIn: {
-          args: [["^[a-z]+$", "!", "#", "*"], ["en"], [0 - 9]],
-          msg: "유효하지 않은 문자가 포함되어있습니다.",
-        },
-        isAlpha: false,
-      },
     },
     userID: {
       type: Sequelize.STRING(20),
       allowNull: false,
       unique: true,
-      validate: {
-        isIn: {
-          args: [["^[a-z]+$", "!", "#", "*"], ["kr"]],
-          msg: "아이디는 영문과 숫자로만 구성되어야 합니다.",
-        },
-        isLowercase: true,
-        isAlpha: true,
-        isFloat: true,
-      },
     },
     password: {
       type: Sequelize.STRING(20),
       allowNull: false,
-      validate: {
-        notIn: {
-          args: [["A-Z"], ["a-z"], "!", "?", "^", "(", ")", [0 - 9]],
-          msg: "비밀번호는 영문 대소문자와 숫자, 특수문자가 포함되어야 합니다",
-        },
-        isAlpha: true,
-        isInL: {
-          args: ["kr"],
-          msg: "비밀번호는 영문 대소문자와 숫자, 특수문자가 포함되어야 합니다",
-        },
-      },
     },
     email: {
       type: Sequelize.STRING(20),
       allowNull: false,
-      validate: {
-        notIn: {
-          args: ["gmail", "hanmail", "naver"],
-          msg: "유효하지 않은 이메일입니다.",
-        },
-      },
     },
     created_at: {
       type: Sequelize.DATE,
