7e81e25b12a9c5b0782c83e01dee6a9298f3dee2
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-15 16:33:51 +0900

    CREATE sign up presenter

--- src/presenter/SignUpPresenter.jsx
+++ src/presenter/SignUpPresenter.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
-
-import Button from "../components/SubmitBtn";
+import axios from "axios";
 
 const MoveDown = keyframes`
  from {
@@ -21,7 +21,20 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-around;
 `;
-
+const Button = styled.div`
+  width: 150px;
+  height: 40px;
+  border: 1px solid #30475e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Fredericka the Great", cursive;
+  cursor: pointer;
+  &:hover {
+    background-color: #30475e;
+    color: white;
+  }
+`;
 const Title = styled.span`
   font-size: 25px;
   /* font-family: "Fredericka the Great", cursive; */
@@ -83,21 +96,93 @@ const SLink = styled(Link)`
 `;
 
 const SignUpPresenter = () => {
+  const [state, setState] = useState({
+    user: { name: "", id: "", password: "", confirmPassword: "", email: "" },
+  });
+  function onChange(event) {
+    const target = event.target.name;
+    const value = event.target.value;
+    switch (target) {
+      case "name": {
+        return setState({
+          user: { ...state.user, name: value },
+        });
+      }
+      case "password": {
+        return setState({
+          user: { ...state.user, password: value },
+        });
+      }
+      case "confirmPassword": {
+        return setState({
+          user: { ...state.user, confirmPassword: value },
+        });
+      }
+      case "id": {
+        return setState({
+          user: { ...state.user, id: value },
+        });
+      }
+      case "email": {
+        return setState({
+          user: { ...state.user, email: value },
+        });
+      }
+    }
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    const res = await axios.post("/api/user/signUp", state);
+    console.log(res);
+  }
   return (
     <Container>
       <Title>Sign Up</Title>
       <Wrapper>
         <Form>
-          <Input type="text" placeholder="ID" />
-          <Input type="password" placeholder="Password" />
+          <Input
+            type="text"
+            placeholder="ID"
+            value={state.user.id}
+            name="id"
+            onChange={onChange}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            value={state.user.password}
+            autocomplete="off"
+            name="password"
+            onChange={onChange}
+          />
           <Info>* 영문 대소문자, 숫자, 특수문자를 포함한 10자리 이상</Info>
-          <Input type="password" placeholder="Confirm Password" />
-          <Input type="text" placeholder="Name" />
-          <Input type="email" placeholder="Email" />
+          <Input
+            type="password"
+            placeholder="Confirm Password"
+            value={state.user.confirmPassword}
+            autocomplete="off"
+            name="confirmPassword"
+            onChange={onChange}
+          />
+          <Input
+            type="text"
+            placeholder="Name"
+            value={state.user.name}
+            name="name"
+            onChange={onChange}
+          />
+          <Input
+            type="email"
+            placeholder="Email"
+            value={state.user.email}
+            name="email"
+            onChange={onChange}
+          />
         </Form>
       </Wrapper>
       <Section>
-        <Button />
+        <Button onClick={onSubmit}>Submit</Button>
         <SLink to="/">
           <CancelBtn>Cancel</CancelBtn>
         </SLink>
