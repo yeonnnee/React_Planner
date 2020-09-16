db465e64cbe8ad770c40167d2f403a015d7c608d
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-16 16:58:48 +0900

    CREATE account successfully created message

---
+++ src/components/SuccessSignUp.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import WelcomeMessage from "./WelcomeMessage";
+
+const SLink = styled(Link)`
+  text-decoration: none;
+  color: #382933;
+`;
+
+const Button = styled.div`
+  width: 150px;
+  height: 40px;
+  border: 1px solid #30475e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Life Savers", cursive;
+  cursor: pointer;
+  &:hover {
+    background-color: #30475e;
+    color: white;
+  }
+`;
+const Text = styled.div`
+  font-size: 18px;
+  font-family: "Life Savers", cursive;
+`;
+const Container = styled.div`
+  height: 50%;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const SuccessSignUp = () => {
+  return (
+    <Container>
+      <WelcomeMessage />
+      <Text>Account Successfuly Created</Text>
+      <p>로그인 하시겠습니까?</p>
+      <SLink to="/logIn">
+        <Button>LogIn</Button>
+      </SLink>
+    </Container>
+  );
+};
+
+export default SuccessSignUp;
