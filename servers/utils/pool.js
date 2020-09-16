97c577d29e3ef23bfd3bbd0b661b9d29e83d5d02
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-16 23:23:25 +0900

    CREATE pool

---
+++ servers/utils/pool.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql2");
+const configs = require("./configs");
+
+const pool = mysql.createPool({
+  host: configs.host,
+  user: configs.username,
+  database: configs.database,
+  password: configs.password,
+  port: configs.port,
+});
+
+pool.getConnection(function (err, connection) {
+  if (!err) {
+    connection.query("SELECT * FROM users", function (error, result) {
+      if (error) {
+        throw error;
+      } else {
+        console.log(result);
+        connection.release();
+      }
+    });
+  }
+});
+
+module.exports = pool;
