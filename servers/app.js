323bc7600940a6a3e0131fd486bf1ca7d9846cc1
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-16 23:24:45 +0900

    CREATE session

--- servers/app.js
+++ servers/app.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const session = require("express-session");
+const MysqlStore = require("express-mysql-session")(session);
 
-// const { sequelize } = require("./models");
-// const { configs } = require("../utils/configs");
 const taskRoutes = require("./routes/taskRoutes");
 const userRoutes = require("./routes/userRoutes");
+const sequelize = require("./utils/database");
+const config = require("./utils/configs");
 
 const app = express();
+sequelize.sync();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(
+  session({
+    secret: config.session_secret,
+    resave: false,
+    saveUninitialized: false,
+    store: new MysqlStore({
+      host: config.host,
+      user: config.username,
+      password: config.password,
+      database: config.database,
+    }),
+  })
+);
+
 app.use("/api/user", userRoutes);
 app.use("/api", taskRoutes);
 
-// const driver = async () => {
-//   await sequelize.sync();
-// };
-
-// driver();
+const driver = async () => {
+  await sequelize.sync();
+};
+driver();
 
 app.listen(3001, () => console.log("hello from the server"));
