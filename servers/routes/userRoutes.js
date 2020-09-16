39cb628732a4863eaca0688d12237e0d3c4eb1af
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-17 00:31:14 +0900

    CREATE validator to verify existing userID and email

--- servers/routes/userRoutes.js
+++ servers/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { body } = require("express-validator");
 
 const signUpCon = require("../controllers/signUpCon");
+const User = require("../models/user");
 router.get("/signUp", signUpCon.getData);
 router.post(
   "/signUp",
@@ -11,6 +12,14 @@ router.post(
       .trim()
       .isLength({ min: 6 })
       .withMessage("아이디는 6글자 이상의 영문과 숫자로 구성되어야 합니다")
+      .bail()
+      .custom(async (value) => {
+        const findUser = await User.findOne({ where: { userID: value } });
+        if (findUser) {
+          throw new Error("이미 존재하는 아이디입니다");
+        }
+        return true;
+      })
       .bail()
       .custom((value) => {
         const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
@@ -24,6 +33,7 @@ router.post(
       .trim()
       .isLength({ min: 8 })
       .withMessage("비밀번호는 8~20자로 구성되어야 합니다.")
+      .bail()
       .custom((value) => {
         const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
         const result = regex.test(value);
@@ -34,6 +44,7 @@ router.post(
         }
         return true;
       })
+      .bail()
       .custom((value) => {
         const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
         const result = korean.test(value);
@@ -56,6 +67,7 @@ router.post(
       .trim()
       .isLength({ max: 6 })
       .withMessage("글자 수가 초과하였습니다.")
+      .bail()
       .custom((value) => {
         const regex = /[#?!@$%^&*-]/;
         const result = regex.test(value);
@@ -64,6 +76,7 @@ router.post(
         }
         return true;
       })
+      .bail()
       .custom((value) => {
         const regex = /[0-9|a-z|A-Z]/;
         const result = regex.test(value);
@@ -72,6 +85,7 @@ router.post(
         }
         return true;
       })
+      .bail()
       .custom((value) => {
         const regex = /[ㄱ-ㅎ|ㅏ-ㅣ|힣|헿|킼|쿅|쿅|홓|횧|홯|탛|챃|훃|킹|큥|쿙|]/;
         const result = regex.test(value);
@@ -87,6 +101,14 @@ router.post(
         const result = regex.test(value);
         if (!result) {
           throw new Error("유효하지 않은 이메일입니다");
+        }
+        return true;
+      })
+      .bail()
+      .custom(async (value) => {
+        const findUser = await User.findOne({ where: { email: value } });
+        if (findUser) {
+          throw new Error("이미 사용중인 이메일입니다");
         }
         return true;
       }),
