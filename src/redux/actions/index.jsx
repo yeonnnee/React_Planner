b5ac79650caf5095faecb7082d77d6ef7c212fe3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-19 00:41:09 +0900

    CREATE actions for user

--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -6,3 +6,15 @@ export const ADD_TASKS = "[TASKS] add new tasks";
 export const DELETE_TASKS = "[TASKS] delete tasks";
 export const CHANGE_STATUS =
   "[TASKS] change task status to FINISHED or PENDING";
+
+export const SET_USER_NAME = "[USER] user name";
+export const SET_USER_ID = "[USER] user ID";
+export const SET_USER_PASSWORD = "[USER] user password";
+export const SET_USER_CONFIRMPW = "[USER] user confirm password";
+export const SET_USER_EMAIL = "[USER] user email";
+
+export const SEND_DATA = "[USER] send data to server";
+export const SEND_DATA_SUCCESS = "[USER] data saved successfully ";
+export const SEND_DATA_FAILED = "[USER] saving data falid ";
+
+export const LOG_IN = "[USER] User log in";
