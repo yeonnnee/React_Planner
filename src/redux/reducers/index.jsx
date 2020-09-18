c3a1bc41079b3223c822bd71ef2cfb639d07e1e3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-19 00:41:37 +0900

    CREATE user reducer

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import tasksReducer from "./tasksReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   tasksReducer,
+  userReducer,
 });
 
 export default rootReducer;
---
+++ src/redux/reducers/userReducer.jsx
@@ -0,0 +1,87 @@
+import {
+  LOG_IN,
+  SEND_DATA,
+  SEND_DATA_FAILED,
+  SEND_DATA_SUCCESS,
+  SET_USER_CONFIRMPW,
+  SET_USER_EMAIL,
+  SET_USER_ID,
+  SET_USER_NAME,
+  SET_USER_PASSWORD,
+} from "../actions";
+
+export const initialState = {
+  user: {
+    userID: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+    email: "",
+  },
+  isLoading: true,
+  error: "",
+  result: "",
+  logIn: false,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_ID:
+      return {
+        ...state,
+        isLoading: false,
+        user: { ...state.user, userID: action.payload },
+      };
+    case SET_USER_PASSWORD:
+      return {
+        ...state,
+        isLoading: false,
+        user: { ...state.user, password: action.payload },
+      };
+    case SET_USER_CONFIRMPW:
+      return {
+        ...state,
+        isLoading: false,
+        user: { ...state.user, confirmPassword: action.payload },
+      };
+    case SET_USER_NAME:
+      return {
+        ...state,
+        isLoading: false,
+        user: { ...state.user, name: action.payload },
+      };
+    case SET_USER_EMAIL:
+      return {
+        ...state,
+        isLoading: false,
+        user: { ...state.user, email: action.payload },
+      };
+    case SEND_DATA:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case SEND_DATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        result: "SUCCESS",
+      };
+    case SEND_DATA_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        result: "FAILED",
+        error: action.payload,
+      };
+    case LOG_IN:
+      return {
+        ...state,
+        isLoading: false,
+        logIn: true,
+        result: "SUCCESS",
+      };
+  }
+};
+
+export default userReducer;
