ac14ff2f639ec72a4a2f38ce1b2a62e9f060c58b
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-20 00:36:31 +0900

    CREATE logIn actions

--- src/redux/actions/index.jsx
+++ src/redux/actions/index.jsx
@@ -1,3 +1,4 @@
+/* TASKS ACTIONS */
 export const FETCH_START = "[TASKS] fetching data start";
 export const FETCH_SUCCESS = "[TASKS] fetching data success";
 export const FETCH_FAILED = "[TASKS] fetching data failed";
@@ -7,14 +8,20 @@ export const DELETE_TASKS = "[TASKS] delete tasks";
 export const CHANGE_STATUS =
   "[TASKS] change task status to FINISHED or PENDING";
 
-export const SET_USER_NAME = "[USER] user name";
-export const SET_USER_ID = "[USER] user ID";
-export const SET_USER_PASSWORD = "[USER] user password";
-export const SET_USER_CONFIRMPW = "[USER] user confirm password";
-export const SET_USER_EMAIL = "[USER] user email";
+/* SIGN UP ACTIONS */
+export const SET_USER_NAME = "[SIGN_UP] user name";
+export const SET_USER_ID = "[SIGN_UP] user ID";
+export const SET_USER_PASSWORD = "[SIGN_UP] user password";
+export const SET_USER_CONFIRMPW = "[SIGN_UP] user confirm password";
+export const SET_USER_EMAIL = "[SIGN_UP] user email";
 
-export const SEND_DATA = "[USER] send data to server";
-export const SEND_DATA_SUCCESS = "[USER] data saved successfully ";
-export const SEND_DATA_FAILED = "[USER] saving data falid ";
+export const SEND_DATA = "[SIGN_UP] send data to server";
+export const SEND_DATA_SUCCESS = "[SIGN_UP] data saved successfully ";
+export const SEND_DATA_FAILED = "[SIGN_UP] saving data falid ";
 
-export const LOG_IN = "[USER] User log in";
+/* LOG IN ACTIONS */
+export const SET_LOGIN_ID = "[LOG_IN] set user log-in ID";
+export const SET_LOGIN_PASSWORD = "[LOG_IN] set user log-in PW";
+export const TRY_LOGIN = "[LOG_IN] User log in";
+export const LOGIN_FAILED = "[LOG_IN] failed to log-in";
+export const LOGIN_SUCCESS = "[LOG_IN] Succeed to log-in";
