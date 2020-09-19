875494fda65e97430ac901efd539c3d466bdeb13
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-19 12:51:10 +0900

    ADD error message

--- src/components/userInput/UserEmail.jsx
+++ src/components/userInput/UserEmail.jsx
@@ -1,31 +1,35 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { SET_USER_EMAIL } from "../../redux/actions";
+import { SEND_DATA_FAILED, SET_USER_EMAIL } from "../../redux/actions";
 
-import { Form, Input, Label } from "../../presenter/SignUpPresenter";
+import { Form, Input, Label, Error } from "../../presenter/SignUpPresenter";
 
-const UserEmail = ({ state, save }) => {
+const UserEmail = ({ state, save, setError }) => {
   const [email, setEmail] = useState("");
 
   function onChange(event) {
     const value = event.target.value;
+    setError("");
     setEmail(value);
     save(email);
   }
 
   return (
-    <Form>
-      <Label htmlFor="email">이메일 :</Label>
-      <Input
-        type="email"
-        placeholder="Email"
-        value={state.user.email}
-        id="email"
-        onChange={onChange}
-        error={state.error.includes("이메일") ? true : false}
-      />
-    </Form>
+    <>
+      <Form>
+        <Label htmlFor="email">이메일 :</Label>
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          id="email"
+          onChange={onChange}
+          error={state.error.includes("이메일") ? true : false}
+        />
+      </Form>
+      {state.error.includes("이메일") ? <Error>{state.error}</Error> : null}
+    </>
   );
 };
 
@@ -36,6 +40,9 @@ function mapDispatchToProps(dispatch) {
   return {
     save: (email) => {
       dispatch({ type: SET_USER_EMAIL, payload: email });
+    },
+    setError: (text) => {
+      dispatch({ type: SEND_DATA_FAILED, payload: text });
     },
   };
 }
