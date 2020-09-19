418f5d73b7b4ce6dea32b38d6e925a2e4aca37e8
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-19 12:50:41 +0900

    ADD error message

--- src/components/userInput/ConfirmPW.jsx
+++ src/components/userInput/ConfirmPW.jsx
@@ -1,32 +1,36 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { SET_USER_CONFIRMPW } from "../../redux/actions";
+import { SEND_DATA_FAILED, SET_USER_CONFIRMPW } from "../../redux/actions";
 
-import { Form, Input, Label } from "../../presenter/SignUpPresenter";
+import { Form, Input, Label, Error } from "../../presenter/SignUpPresenter";
 
-const ConfirmPassword = ({ state, save }) => {
+const ConfirmPassword = ({ state, save, setError }) => {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   function onChange(event) {
     const value = event.target.value;
+    setError("");
     setConfirmPassword(value);
     save(confirmPassword);
   }
 
   return (
-    <Form>
-      <Label htmlFor="confirmPassword">비밀번호 확인 :</Label>
-      <Input
-        type="password"
-        placeholder="Confirm Password"
-        value={state.user.confirmPassword}
-        autocomplete="off"
-        id="confirmPassword"
-        onChange={onChange}
-        error={state.error.includes("일치") ? true : false}
-      />
-    </Form>
+    <>
+      <Form>
+        <Label htmlFor="confirmPassword">비밀번호 확인 :</Label>
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          autocomplete="off"
+          id="confirmPassword"
+          onChange={onChange}
+          error={state.error.includes("일치") ? true : false}
+        />
+      </Form>
+      {state.error.includes("일치") ? <Error>{state.error}</Error> : null}
+    </>
   );
 };
 
@@ -37,6 +41,9 @@ function mapDispatchToProps(dispatch) {
   return {
     save: (confirmPassword) => {
       dispatch({ type: SET_USER_CONFIRMPW, payload: confirmPassword });
+    },
+    setError: (text) => {
+      dispatch({ type: SEND_DATA_FAILED, payload: text });
     },
   };
 }
