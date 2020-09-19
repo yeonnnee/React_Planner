9c2bc7a10722faff1ef675327163b8967567640f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-19 12:51:14 +0900

    ADD error message

--- src/components/userInput/UserID.jsx
+++ src/components/userInput/UserID.jsx
@@ -1,33 +1,47 @@
 /* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { SET_USER_ID } from "../../redux/actions";
+import { SEND_DATA_FAILED, SET_USER_ID } from "../../redux/actions";
 
-import { Form, Input, Label } from "../../presenter/SignUpPresenter";
+import {
+  Form,
+  Input,
+  Label,
+  Error,
+  Info,
+} from "../../presenter/SignUpPresenter";
 
-const UserInput = ({ state, save }) => {
+const UserInput = ({ state, save, setError }) => {
   const [userId, setUserId] = useState("");
 
   function onChange(event) {
     const value = event.target.value;
+    setError("");
     setUserId(value);
     save(userId);
   }
 
   return (
-    <Form>
-      <Label htmlFor="userID">아이디 :</Label>
-      <Input
-        type="text"
-        placeholder="ID"
-        value={userId}
-        id="userID"
-        onChange={onChange}
-        error={
-          state.error !== "" && state.error.includes("아이디") ? true : false
-        }
-      />
-    </Form>
+    <>
+      <Form>
+        <Label htmlFor="userID">아이디 :</Label>
+        <Input
+          type="text"
+          placeholder="ID"
+          value={userId}
+          id="userID"
+          onChange={onChange}
+          error={
+            state.error !== "" && state.error.includes("아이디") ? true : false
+          }
+        />
+      </Form>
+      {state.error !== "" && state.error.includes("아이디") ? (
+        <Error>{state.error}</Error>
+      ) : (
+        <Info>* 영문 및 숫자로 조합된 6~20자</Info>
+      )}
+    </>
   );
 };
 
@@ -38,6 +52,9 @@ function mapDispatchToProps(dispatch) {
   return {
     save: (userID) => {
       dispatch({ type: SET_USER_ID, payload: userID });
+    },
+    setError: (text) => {
+      dispatch({ type: SEND_DATA_FAILED, payload: text });
     },
   };
 }
