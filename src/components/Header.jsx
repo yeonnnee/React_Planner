f4c797b48bebb5a1dfb50091a84db9ddb4db0e06
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-21 22:35:50 +0900

    CREATE Header for log out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,65 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import styled from "styled-components";
-import { Link, withRouter } from "react-router-dom";
+import axios from "axios";
+import { connect } from "react-redux";
+import { LOG_OUT, LOG_OUT_FAILED } from "../redux/types";
 
 const Container = styled.div`
-  margin: 50px 0;
-`;
-const List = styled.ul`
-  width: 90%;
-  margin: 0 20px;
-  height: 30px;
-  border-top: 1px solid #2c3e50;
-  border-bottom: 1px solid #2c3e50;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-`;
-const Menu = styled.li`
-  text-decoration: none;
-  color: ${(props) => (props.current ? "#90323d" : "#4b4b4b")};
-  &:hover {
-    color: #90323d;
-  }
-`;
-const SLink = styled(Link)`
-  text-decoration: none;
-  font-weight: bold;
-  font-family: "Zilla Slab", serif;
-  /* &:nth-child(1) {
-    background-color: #55828b;
-  }
-  &:nth-child(2) {
-    background-color: #3b6064;
-  }
-  &:nth-child(3) {
-    background-color: #364958;
-  } */
+  position: absolute;
+  top: 20px;
+  right: 30px;
 `;
 
-export default withRouter(({ location }) => (
-  <>
-    {(location.pathname === "/") |
-    (location.pathname === "/logIn") |
-    (location.pathname === "/sign-up") ? null : (
-      <Container>
-        <List>
-          <SLink to="/tasks">
-            <Menu current={location.pathname === "/tasks"}>Tasks</Menu>
-          </SLink>
-          <SLink to="/monthly">
-            <Menu current={location.pathname === "/monthly"}>Monthly</Menu>
-          </SLink>
-          <SLink to="/record">
-            <Menu current={location.pathname === "/record"}>Record</Menu>
-          </SLink>
-        </List>
-      </Container>
-    )}
-  </>
-));
+const Button = styled.button`
+  width: 100px;
+  height: 40px;
+  outline: none;
+  font-family: "Life Savers", cursive;
+  border: 1px solid #30475e;
+  &:hover {
+    background-color: #30475e;
+    color: white;
+  }
+`;
+const User = styled.span`
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+const Header = ({ state, setError, logOut }) => {
+  async function onClick() {
+    const res = await axios.post("/api/auth/logOut");
+    if (res.data === "User logged out") {
+      logOut();
+    } else {
+      setError(res.data);
+    }
+  }
+  return (
+    <>
+      {state.isAuthenticated ? (
+        <Container>
+          <User>{state.user.userID}</User>
+          <Button onClick={onClick}>로그아웃</Button>
+        </Container>
+      ) : null}
+    </>
+  );
+};
+
+function mapStateToProps(state) {
+  return { state: state.authReducer };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    logOut: () => {
+      dispatch({ type: LOG_OUT });
+    },
+    setError: (error) => {
+      dispatch({ type: LOG_OUT_FAILED, payload: error });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
