e4df90d8e7056c8c1becc5e0bc2e241a9b0f411f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-21 19:45:51 +0900

    SET Loader and ErrorMessage

--- src/container/Tasks/index.jsx
+++ src/container/Tasks/index.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/prop-types */
 import React, { useEffect } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 
 import { FETCH_SUCCESS, FETCH_FAILED, FETCH_START } from "../../redux/types";
 import TasksPresenter from "../../presenter/TasksPresenter";
+import Loader from "../../components/Loader";
+import ErrorMessage from "../../components/ErrorMessage";
 
 const Tasks = ({ state, start, success, failed }) => {
   async function fetchData() {
@@ -13,7 +16,7 @@ const Tasks = ({ state, start, success, failed }) => {
 
       success(fetchData.data);
     } catch (error) {
-      failed(error);
+      failed("문제가 발생하였습니다. 잠시 후 다시 시도해주십시오");
     }
   }
 
@@ -21,11 +24,13 @@ const Tasks = ({ state, start, success, failed }) => {
     fetchData();
   }, []);
 
-  if (state.isLoading) {
-    return <h1>Loading...</h1>;
-  } else {
-    return <TasksPresenter {...state} />;
-  }
+  return (
+    <>
+      {state.isLoading ? <Loader /> : null}
+      {state.error !== "" ? <ErrorMessage {...state} /> : null}
+      <TasksPresenter {...state} />
+    </>
+  );
 };
 
 function mapStateToProps(state) {
