ea396bfd9383d6b89cdf1732985813ddcd351ca7
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-21 23:38:57 +0900

    combine login user id & pw input with index container

--- src/container/LogIn/index.jsx
+++ src/container/LogIn/index.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 
-import UserID from "./LogID";
-import Password from "./LogInPW";
 import { TRY_LOGIN, LOGIN_FAILED, LOGIN_SUCCESS } from "../../redux/types";
+import Loader from "../../components/Loader";
 import Header from "../../components/WelcomeMessage";
 import ErrorMessage from "../../components/ErrorMessage";
 import {
@@ -13,16 +12,32 @@ import {
   Section,
   Wrapper,
   Button,
+  Form,
+  Input,
 } from "../../presenter/LogInPresenter";
-import Loader from "../../components/Loader";
 
 const LogIn = ({ state, send, setError, success, history }) => {
+  const [user, setUser] = useState({ userID: "", password: "" });
+
+  function onChange(event) {
+    const target = event.target;
+    if (target.name === "userID") {
+      const value = target.value;
+      setUser({ ...user, userID: value });
+    }
+    if (target.name === "password") {
+      const value = target.value;
+      setUser({ ...user, password: value });
+    }
+  }
+
   async function logIn() {
-    if (state.user.userID === "" || state.user.password === "") {
+    if (user.userID === "" || user.password === "") {
       setError("아이디와 비밀번호를 입력해주시기 바랍니다");
     } else {
       send();
-      const res = await axios.post("/api/auth/logIn", state.user);
+      setError("");
+      const res = await axios.post("/api/auth/logIn", user);
       console.log(res);
 
       if (res.data !== "logged In successfully") {
@@ -39,9 +54,26 @@ const LogIn = ({ state, send, setError, success, history }) => {
         <Header />
         <Wrapper>
           {state.isLoading ? <Loader /> : null}
-          <ErrorMessage {...state} />
-          <UserID />
-          <Password />
+          {state.error !== "" ? <ErrorMessage {...state} /> : null}
+
+          <Form>
+            <Input
+              type="text"
+              placeholder="ID"
+              name="userID"
+              value={user.userID}
+              onChange={onChange}
+            />
+          </Form>
+          <Form>
+            <Input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={user.password}
+              onChange={onChange}
+            />
+          </Form>
         </Wrapper>
         <Section>
           <Button onClick={logIn}>Log In</Button>
