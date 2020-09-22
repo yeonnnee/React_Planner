2eac69e3898e2c5f8a674259bca1e0e0ce84acc1
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-22 22:53:34 +0900

    CREATE monthly route

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Tasks from "../container/Tasks";
+import Monthly from "../container/Monthly";
 import Home from "../container/Home";
 import LogIn from "../container/LogIn";
 import SignUp from "../container/SignUp";
@@ -16,10 +17,11 @@ export default () => {
       <Header />
       <Navigation />
       <Switch>
-        <PrivateRoute exact path="/tasks" component={Tasks} />
         <Route path="/" exact component={Home} />
         <Route path="/logIn" component={LogIn} />
         <Route path="/sign-up" component={SignUp} />
+        <PrivateRoute exact path="/tasks" component={Tasks} />
+        <PrivateRoute exact path="/monthly" component={Monthly} />
       </Switch>
     </Router>
   );
