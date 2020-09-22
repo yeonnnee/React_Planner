d523272ab6f9ae67e6236151034c583e46749cea
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-22 22:53:11 +0900

    refactoring and organze filse

---
+++ src/container/SignUp/Input.jsx
@@ -0,0 +1,85 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  height: 60px;
+  width: 100%;
+  margin: 5px 30px;
+  display: grid;
+  grid-template-rows: 40px;
+  grid-template-columns: 105px 400px;
+  gap: 10px;
+`;
+const Label = styled.label`
+  font-size: 13px;
+  width: 105px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const Input = styled.input`
+  width: 60%;
+  padding: 10px 20px;
+  outline: none;
+  border: ${(props) => (props.error ? "1px solid red" : "none")};
+  border-radius: 5px;
+  &:focus {
+    border: 2px solid #20639b;
+  }
+`;
+const Error = styled.div`
+  width: 60%;
+  margin: 0 30%;
+  font-size: 11px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  top: -18px;
+  color: red;
+  line-height: 1.5;
+`;
+const Info = styled.div`
+  width: 60%;
+  margin: 0 31%;
+  font-size: 11px;
+  display: flex;
+  line-height: 1.5;
+  align-items: center;
+  position: relative;
+  top: -18px;
+`;
+const SignUpInput = ({
+  onChange,
+  type,
+  placeholder,
+  error,
+  errorMessage,
+  value,
+  id,
+}) => {
+  return (
+    <>
+      <Form>
+        <Label htmlFor={id}>{id} :</Label>
+        <Input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          id={id}
+          onChange={onChange}
+          error={error}
+        />
+      </Form>
+      {id === "아이디" ? (
+        <Info>*영문 혹은 영문과 숫자로 조합된 6~20자</Info>
+      ) : null}
+      {id === "비밀번호" ? (
+        <Info>*영문 대소문자, 숫자, 특수문자를 포함한 8~20자</Info>
+      ) : null}
+      {error ? <Error>{errorMessage}</Error> : null}
+    </>
+  );
+};
+
+export default SignUpInput;
