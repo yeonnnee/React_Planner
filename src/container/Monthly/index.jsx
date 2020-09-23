34921d1145f9fc1a748283f728f50f33c8ebf69e
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-23 18:12:44 +0900

    CREATE monthly container

--- src/container/Monthly/index.jsx
+++ src/container/Monthly/index.jsx
@@ -1,66 +1,27 @@
-import React from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MonthlyPresenter from "../../presenter/MonthlyPresenter";
 
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const MonthlyCalendar = styled(Calendar)`
-  width: 100%;
-  padding: 10px;
-`;
-
-const Line = styled.div`
-  width: 90%;
-  margin-top: 20px;
-  height: 5px;
-  border-bottom: 1px solid #30475e;
-`;
-
-const Button = styled.button`
-  width: 80px;
-  height: 30px;
-  margin: 5px 23px;
-  outline: none;
-  border: 1px solid #30475e;
-  background: none;
-  cursor: pointer;
-  font-family: "Life Savers", cursive;
-  &:hover {
-    background-color: #30475e;
-    color: white;
-  }
-`;
-
-const RecordLink = styled(Link)`
-  text-decoration: none;
-  align-self: flex-end;
-`;
 const Monthly = () => {
-  const onClickDay = (event) => {
-    const target = event.toString().substring(0, 15);
-    console.log(target);
-  };
-  return (
-    <Container>
-      <MonthlyCalendar
-        calendarType={"US"}
-        onClickDay={onClickDay}
-        locale="en-US"
-      />
-      <Line />
-      <RecordLink to="/monthly">
-        <Button>ADD</Button>
-      </RecordLink>
-    </Container>
-  );
+  const [state, setState] = useState({
+    date: "",
+    content: "",
+    id: "",
+    createdAt: "",
+  });
+
+  async function fetchData() {
+    try {
+      const res = await axios.get("/api/monthly");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return <MonthlyPresenter />;
 };
 
 export default Monthly;
