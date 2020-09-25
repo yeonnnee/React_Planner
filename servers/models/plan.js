bcd1fc2a5b691c4f6fadbf4ab01970a3e8efa9ac
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-26 01:35:34 +0900

    define plan model

--- servers/models/plan.js
+++ servers/models/plan.js
@@ -4,13 +4,13 @@ const sequelize = require("./index");
 const Plan = sequelize.define(
   "plans",
   {
+    id: {
+      type: Sequelize.STRING(36),
+      allowNull: false,
+    },
     date: {
       type: Sequelize.STRING(16),
       allowNull: false,
-    },
-    writer: {
-      type: Sequelize.STRING(15),
-      allowNull: true,
     },
   },
   {
