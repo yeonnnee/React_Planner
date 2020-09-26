0ffae399d2325647a201ae85ed80c1055d9c22f3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-27 02:55:03 +0900

    CREATE monthly controller

---
+++ servers/controllers/monthlyCon.js
@@ -0,0 +1,37 @@
+const Plan = require("../models/plan");
+const Content = require("../models/content");
+
+exports.getMonthly = async (req, res) => {
+  try {
+    const userID = await req.session.user.userID;
+    const plans = await Plan.findAll({ where: { writer: userID } });
+    res.status(200).send(plans);
+  } catch (error) {
+    throw new Error();
+  }
+};
+
+exports.postMonthly = async (req, res) => {
+  try {
+    // const user = await req.session.user;
+    // const writer = user.useID;
+    const monthlyId = req.body.id;
+    const date = req.body.date;
+    Plan.create({ id: monthlyId, date: date });
+    // res.send("Get data successfully");
+    res.status(201).json({ msg: "Get data successfully" });
+  } catch (error) {
+    throw new Error();
+  }
+};
+
+exports.postContent = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const text = req.body.text;
+    Content.create({ id: id, text: text });
+    res.status(201).json({ msg: "Create content successfully" });
+  } catch (error) {
+    throw new Error();
+  }
+};
