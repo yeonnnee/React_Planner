93896823f239a85d9b97198b228fa0d9b04d56b3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-27 18:24:10 +0900

    CREATE signUp validation and tasks validation

---
+++ servers/routes/validations/signUpValidation.js
@@ -0,0 +1,92 @@
+const { body } = require("express-validator");
+
+const User = require("../../models/user");
+
+const signUpValidation = [
+  body("email")
+    .trim()
+    .custom((value) => {
+      const regex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[hanmail|naver|gmail]*.[com|net]{2,3}$/i;
+      const result = regex.test(value);
+      if (!result) {
+        throw new Error("유효하지 않은 이메일입니다");
+      }
+      return true;
+    })
+    .bail()
+    .custom(async (value) => {
+      const findUser = await User.findOne({ where: { email: value } });
+      if (findUser) {
+        throw new Error("이미 사용중인 이메일입니다");
+      }
+      return true;
+    }),
+
+  body("password")
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage("비밀번호는 8~12자로 구성되어야 합니다.")
+    .bail()
+    .custom((value) => {
+      const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+      const result = regex.test(value);
+      if (!result) {
+        throw new Error(
+          "비밀번호는 영문 대소문자, 숫자, 특수문자(#?!@$%^&*-)를 포함한 8~12자로 입력해주십시오."
+        );
+      }
+      return true;
+    })
+    .bail()
+    .custom((value) => {
+      const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+      const result = korean.test(value);
+      if (result) {
+        throw new Error(
+          "비밀번호는 영문 대소문자, 숫자, 특수문자(#?!@$%^&*-)를 포함한 8~12자로 입력해주십시오."
+        );
+      }
+      return true;
+    }),
+  body("confirmPassword")
+    .trim()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("비밀번호가 일치하지 않습니다");
+      }
+      return true;
+    }),
+  body("name")
+    .trim()
+    .isLength({ max: 6 })
+    .withMessage("글자 수가 초과하였습니다.")
+    .bail()
+    .custom((value) => {
+      const regex = /[#?!@$%^&*-]/;
+      const result = regex.test(value);
+      if (result) {
+        throw new Error("한글로 입력해주십시오.");
+      }
+      return true;
+    })
+    .bail()
+    .custom((value) => {
+      const regex = /[0-9|a-z|A-Z]/;
+      const result = regex.test(value);
+      if (result) {
+        throw new Error("한글로 입력해주십시오.");
+      }
+      return true;
+    })
+    .bail()
+    .custom((value) => {
+      const regex = /[ㄱ-ㅎ|ㅏ-ㅣ|핳|힣|캌|킼|핰|힠|캏|킿|쿟|쿅|쿜|훃|홓|홐|훜|쿸|큨|흏]/;
+      const result = regex.test(value);
+      if (result) {
+        throw new Error("유효하지 않은 글자가 포함되어 있습니다.");
+      }
+      return true;
+    }),
+];
+
+module.exports = signUpValidation;
