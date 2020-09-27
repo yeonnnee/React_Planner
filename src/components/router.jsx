10cbc8dd005caa937c9808f3477efb793b94f51b
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-28 00:45:32 +0900

    REVISE path and create monthlyAdd route

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Tasks from "../container/Tasks";
-import Monthly from "../container/Monthly";
-import Home from "../container/Home";
-import LogIn from "../container/LogIn";
-import SignUp from "../container/SignUp";
+import Tasks from "../routes/Tasks";
+import Monthly from "../routes/Monthly";
+import AddMonthly from "../routes/Monthly/AddMonthly";
+import Home from "../routes/Home";
+import LogIn from "../routes/LogIn";
+import SignUp from "../routes/SignUp";
 import Header from "./Header";
 import Navigation from "./Navigation";
 import PrivateRoute from "./PrivateRoute";
@@ -22,6 +23,10 @@ export default () => {
         <Route path="/sign-up" component={SignUp} />
         <PrivateRoute exact path="/tasks" component={Tasks} />
         <PrivateRoute exact path="/monthly" component={Monthly} />
+        <PrivateRoute exact path="/monthly/add" component={AddMonthly} />
+
+        {/* <Route path="/" exact component={Monthly} />
+        <Route path="/add" component={AddMonthly} /> */}
       </Switch>
     </Router>
   );
