48aea8864654e0aac57f3dc78b06aba5815c7b92
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-29 01:02:27 +0900

    CREATE sendData function seperate from onSubmit function to avoid send data to server even value has an validation error

--- src/routes/SignUp/index.jsx
+++ src/routes/SignUp/index.jsx
@@ -3,24 +3,24 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
-import SuccessMessage from "../../components/msg/SuccessMessage";
 import SignUpPresenter from "./SignUpPresenter";
 import {
   emailValidation,
   passwordValidation,
   confirmPw_validation,
   nameValidation,
-  validation,
 } from "./validation";
 
 import {
   SEND_DATA,
   SEND_DATA_FAILED,
   SEND_DATA_SUCCESS,
+  VALIDATION_ERROR,
 } from "../../redux/types";
 
 const SignUp = (signUpProps) => {
-  const { state, send, setError, success } = signUpProps;
+  const { state, send, failed, success, setError, history } = signUpProps;
+
   const [userInfo, setUserInfo] = useState({
     id: uuidv4().toString(),
     email: "",
@@ -28,14 +28,17 @@ const SignUp = (signUpProps) => {
     confirmPassword: "",
     name: "",
   });
+
   function onChange(event) {
     const target = event.target.placeholder;
+
     switch (target) {
       case "Password": {
         const value = event.target.value;
         passwordValidation(value, setError);
         return setUserInfo({ ...userInfo, password: value });
       }
+
       case "Confirm Password": {
         const value = event.target.value;
         confirmPw_validation(value, userInfo.password, setError);
@@ -44,11 +47,13 @@ const SignUp = (signUpProps) => {
           confirmPassword: value,
         });
       }
+
       case "Name": {
         const value = event.target.value;
         nameValidation(value, setError);
         return setUserInfo({ ...userInfo, name: value });
       }
+
       case "Email": {
         const value = event.target.value;
         emailValidation(value, setError);
@@ -61,46 +66,67 @@ const SignUp = (signUpProps) => {
         return userInfo;
     }
   }
-
-  async function onSubmit(event) {
-    event.preventDefault();
-
+  async function sendData() {
     try {
-      validation(
-        userInfo.email,
-        userInfo.password,
-        userInfo.confirmPassword,
-        userInfo.name,
-        setError
-      );
       send();
       const res = await axios.post("/api/user/signUp", userInfo);
       const result = res.data.msg;
+
       if (result === "Get data successfully") {
         success();
+        history.push("/sign-up/success");
       }
     } catch (error) {
       // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
       if (error) {
-        const res = error.response.data.msg;
-        setError(res);
+        if (error.response.status === 400) {
+          const res = error.response.data;
+          switch (res.param) {
+            case "email":
+              return setError({ email: res.msg });
+            case "password":
+              return setError({ password: res.msg });
+            case "name":
+              return setError({ name: res.msg });
+            default:
+              failed();
+          }
+        } else {
+          console.log(error.response.status);
+        }
       }
+    }
+  }
+  function onSubmit(event) {
+    event.preventDefault();
+
+    if (
+      userInfo.email ||
+      userInfo.name ||
+      userInfo.password ||
+      userInfo.confirmPassword === ""
+    ) {
+      return failed("빈칸 없이 입력해주십시오");
+    } else {
+      failed("");
+    }
+    if (
+      state.validation.email ||
+      state.validation.pw ||
+      state.validation.confirmPw ||
+      state.validation.name === ""
+    ) {
+      sendData();
     }
   }
 
   return (
-    <>
-      {state.result === "SUCCESS" ? (
-        <SuccessMessage />
-      ) : (
-        <SignUpPresenter
-          onSubmit={onSubmit}
-          onChange={onChange}
-          {...state}
-          {...userInfo}
-        />
-      )}
-    </>
+    <SignUpPresenter
+      onSubmit={onSubmit}
+      onChange={onChange}
+      userInfo={userInfo}
+      {...state}
+    />
   );
 };
 
@@ -112,11 +138,14 @@ function mapDispatchToProps(dispatch) {
     send: () => {
       dispatch({ type: SEND_DATA });
     },
-    setError: (error) => {
+    failed: (error) => {
       dispatch({ type: SEND_DATA_FAILED, payload: error });
     },
     success: () => {
       dispatch({ type: SEND_DATA_SUCCESS });
+    },
+    setError: (error) => {
+      dispatch({ type: VALIDATION_ERROR, payload: error });
     },
   };
 }
