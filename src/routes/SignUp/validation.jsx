09340a0d1b7d9fea2ac8e71d7551bdc7a8304616
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-09-29 00:58:55 +0900

    CHANGE ERROR structure

--- src/routes/SignUp/validation.jsx
+++ src/routes/SignUp/validation.jsx
@@ -1,41 +1,33 @@
 export const emailValidation = (val, setError) => {
   if (!val.includes("@")) {
-    setError("유효하지 않은 이메일입니다.");
+    setError({ email: "유효하지 않은 이메일입니다." });
   } else if (val.length > 80) {
-    setError("이메일은 80자 이내로 작성해주십시오.");
+    setError({ email: "이메일은 80자 이내로 작성해주십시오." });
   } else {
-    setError("");
+    setError({ email: "" });
   }
 };
 
 export const passwordValidation = (val, setError) => {
   if (val.length < 8) {
-    setError("비밀번호는 8~12자로 작성해주십시오.");
-  } else if (val.length > 8) {
-    setError("");
+    setError({ password: "비밀번호는 8~12자로 작성해주십시오." });
+  } else {
+    setError({ password: "" });
   }
 };
 
 export const confirmPw_validation = (val, pw, setError) => {
   if (val !== pw) {
-    setError("비밀번호와 일치하지 않습니다.");
+    setError({ confirmPw: "비밀번호와 일치하지 않습니다." });
   } else {
-    setError("");
+    setError({ confirmPw: "" });
   }
 };
 
 export const nameValidation = (val, setError) => {
   if (val.length > 20) {
-    setError("글자 수를 초과하였습니다.");
+    setError({ name: "글자 수를 초과하였습니다." });
   } else {
-    setError("");
-  }
-};
-
-export const validation = (email, pw, confirmPw, name, setError) => {
-  if (email === "" || pw === "" || confirmPw === "" || name === "") {
-    setError("빈칸 없이 입력해주시기 바랍니다");
-  } else {
-    setError("");
+    setError({ name: "" });
   }
 };
