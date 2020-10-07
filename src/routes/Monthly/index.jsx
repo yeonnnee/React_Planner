e891d2f010404a1ad4c5339fd6868d5a939b8b1f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-08 02:15:22 +0900

    CREATE select_monthly action to show the plan which matched with clicked date

--- src/routes/Monthly/index.jsx
+++ src/routes/Monthly/index.jsx
@@ -2,22 +2,34 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 
 import { monthlyApi } from "../../api";
+import MonthlyPresenter from "./MonthlyPresenter";
 import {
   FETCH_MONTHLY_FAILED,
   FETCH_MONTHLY_START,
   FETCH_MONTHLY_SUCCESS,
+  SELECT_MONTHLY,
 } from "../../redux/types";
-import MonthlyPresenter from "./MonthlyPresenter";
 
 const Monthly = (monthlyProps) => {
-  const { state, fetch_monthly, fetch_success, fetch_failed } = monthlyProps;
-  // const [selected, setSelected] = useState();
+  const {
+    state,
+    fetch_monthly,
+    fetch_success,
+    fetch_failed,
+    select,
+  } = monthlyProps;
+
+  const onClickDay = (event) => {
+    const target = event.toString().substring(0, 10);
+    select(target);
+  };
 
   async function fetchData() {
     fetch_monthly();
     try {
       const res = await monthlyApi.getMonthly();
       fetch_success(res.data.monthly);
+      console.log(res);
     } catch (error) {
       console.log(error.response);
       fetch_failed("문제가 발생하였습니다. 잠시 후 다시 시도해주십시오");
@@ -27,7 +39,7 @@ const Monthly = (monthlyProps) => {
   useEffect(() => {
     fetchData();
   }, []);
-  return <MonthlyPresenter {...state} />;
+  return <MonthlyPresenter {...state} onClickDay={onClickDay} />;
 };
 
 function mapStateToProps(state) {
@@ -44,6 +56,9 @@ function mapDispatchToProps(dispatch) {
     },
     fetch_failed: (error) => {
       dispatch({ type: FETCH_MONTHLY_FAILED, payload: error });
+    },
+    select: (date) => {
+      dispatch({ type: SELECT_MONTHLY, payload: date });
     },
   };
 }
