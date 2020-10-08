e26306bc3ab889ac87fb595cc4304f8f5f86a67d
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-09 00:02:18 +0900

    STYLE list for monthly

--- src/routes/Monthly/MonthlyList.jsx
+++ src/routes/Monthly/MonthlyList.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 const List = styled.li`
-  width: 80%;
+  width: 100%;
   height: 70px;
   border: 1px solid #30475e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Life Savers", cursive;
+  background-color: #30475e;
+  color: white;
 `;
 
 const Title = styled.div``;
