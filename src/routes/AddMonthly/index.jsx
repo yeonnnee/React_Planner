68c1b5a0add7981170f3469f7284eb9974e39b01
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-08 16:23:18 +0900

    CREATE deleteItem function

--- src/routes/AddMonthly/index.jsx
+++ src/routes/AddMonthly/index.jsx
@@ -3,65 +3,71 @@ import { connect } from "react-redux";
 import { v4 as uuidv4 } from "uuid";
 
 import AddMonthlyPresenter from "./AddMonthlyPresenter";
-import { CREATE_MONTHLY } from "../../redux/types";
 
 const MonthlyAdd = (monthlyAddProps) => {
-  const { add, history } = monthlyAddProps;
+  const { state, select, history } = monthlyAddProps;
 
-  const [plans, setPlans] = useState({ monthly: [] });
+  const [planList, setPlanList] = useState([]);
   const [content, setContent] = useState({
     id: "",
     text: "",
+    date: "",
     error: "",
   });
 
-  function deleteItem() {
-    console.log("plans", plans.monthly);
+  function save() {
+    // history.push("/monthly");
+    console.log(planList);
   }
 
-  function save() {
-    const date = new Date();
-    const today = date.toString().substring(0, 10);
-    add({
-      id: uuidv4().toString(),
-      date: today,
-      content: [...plans.monthly],
-    });
-    history.push("/");
+  function deleteItem(event) {
+    const target = event.target.parentNode.id;
+    const filteredPlanList = planList.filter((plan) => plan.id !== target);
+    setPlanList([...filteredPlanList]);
   }
 
   function onSubmit(event) {
     event.preventDefault();
-    setPlans({ monthly: [...plans.monthly, content] });
-    setContent({
-      text: "",
-      error: "",
-    });
+    if (content.text !== "") {
+      setPlanList([...planList, content]);
+      setContent({
+        text: "",
+        date: "",
+        error: "",
+      });
+    }
   }
 
   function onChange(event) {
     const value = event.target.value;
-    setContent({ id: uuidv4().toString(), text: value, error: "" });
+    setContent({
+      id: uuidv4().toString(),
+      text: value,
+      date: state.date,
+      error: "",
+    });
   }
 
   return (
     <AddMonthlyPresenter
       onChange={onChange}
       onSubmit={onSubmit}
-      save={save}
       deleteItem={deleteItem}
-      plans={plans}
+      save={save}
+      select={select}
+      planList={planList}
       content={content}
+      {...state}
     />
   );
 };
 
-function mapDispatchToProps(dispatch) {
-  return {
-    add: (monthly) => {
-      dispatch({ type: CREATE_MONTHLY, payload: monthly });
-    },
-  };
+function mapStateToProps(state) {
+  return { state: state.monthlyReducer };
 }
 
-export default connect(null, mapDispatchToProps)(MonthlyAdd);
+function mapDispatchToProps(dispatch) {
+  return {};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MonthlyAdd);
