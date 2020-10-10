50f169de50bf09efbc06679373c2e46efaf0a03f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-10 21:06:38 +0900

    CREATE detail section with btn for delete and edit

--- src/routes/Monthly/MonthlyList.jsx
+++ src/routes/Monthly/MonthlyList.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 
+const SlideDown = keyframes`
+  from {
+    transform: translateY(-30px)
+  } 
+  to{
+    transform: translateY(0);
+  }
+`
 
 const Container = styled.div`
   width: 100%;
-  height: 100px;
   display: flex;
   flex-direction: column;
   align-items: center;
- 
 `;
+
+const Section = styled.div`
+  width: 100%;
+  padding: 10px;
+  display:none;
+  &:hover {
+    display:block;
+  }
+`;
+
 const List = styled.li`
   width: 100%;
   height: 70px;
@@ -20,13 +36,40 @@ const List = styled.li`
   font-family: "Life Savers", cursive;
   background-color: #30475e;
   color: white;
+  z-index:1;
+  &:hover ~ ${Section} {
+    display:block;
+    animation: ${SlideDown} 0.5s linear forwards;
+  }
 `;
 
 const Title = styled.div``;
 
+
 const Detail = styled.ul`
 `;
 const Desc = styled.li``;
+const BtnSection = styled.div`
+  width: 100%;
+  height: 50px;
+  display:flex;
+  justify-content:center;
+  margin-top: 20px;
+`;
+const Btn = styled.button`
+  width: 100px;
+  height: 30px;
+  border-radius: 20px;
+  outline:none;
+  border: 1px solid #30475e;
+  margin-right: 10px;
+  font-family: "Life Savers", cursive;
+  cursor: pointer;
+  &:hover{
+    background-color:#30475e;
+    color: white;
+  }
+`;
 
 
 const MonthlyList = (monthlyProps) => {
@@ -37,11 +80,17 @@ const MonthlyList = (monthlyProps) => {
       <List>
         <Title>{date}</Title>
       </List>
+      <Section>
       <Detail>
         {contents.map((content, index) => {
           return<Desc key={index}>{content.text}</Desc>
         })}
-      </Detail>
+        </Detail>
+        <BtnSection>
+          <Btn>Edit</Btn>
+          <Btn>Delete</Btn>
+        </BtnSection>
+      </Section> 
     </Container>
   );
 };
