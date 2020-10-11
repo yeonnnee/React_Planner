f36f9472b9ecdb42f8575d8c70381c317b229815
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-11 16:07:09 +0900

    SET eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true,
-        "node": true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+  },
 };
