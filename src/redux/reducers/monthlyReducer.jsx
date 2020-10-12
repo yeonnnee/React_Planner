dec79fd6bde84d77e808c96d60e3e94ee9a887be
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-12 19:50:02 +0900

    CREATE change monthYear action to filter list according to month Year

--- src/redux/reducers/monthlyReducer.jsx
+++ src/redux/reducers/monthlyReducer.jsx
@@ -23,6 +23,7 @@ const initialState = {
 
 const monthlyReducer = (state = initialState, action) => {
   switch (action.type) {
+    //* FETCH DATA ACTION *//
     case FETCH_MONTHLY_START: {
       const date = new Date().toString();
       const today = date.substring(0, 15);
@@ -32,9 +33,11 @@ const monthlyReducer = (state = initialState, action) => {
       if (action.payload === []) {
         return { ...state, isLoading: false };
       } else {
+        // DISPLAY TODAY'S PLAN AS DEFAULT //
         const selected_plan = action.payload.filter(
           (plan) => plan.date === state.date
         );
+        // DISPLAY OTHERS //
         const unSelected_plan = action.payload.filter((plan) => {
           const selectedDate = state.date.split(" ");
           const planDate = plan.date.split(" ");
@@ -46,6 +49,7 @@ const monthlyReducer = (state = initialState, action) => {
           );
         });
 
+        // SORTED BY DATE //
         unSelected_plan.sort(function (a, b) {
           const day1 = a.date.substring(9, 10);
           const day2 = b.date.substring(9, 10);
@@ -60,9 +64,13 @@ const monthlyReducer = (state = initialState, action) => {
         };
       }
     }
+
+    //* ACTIVE WHEN ERRORS HAPPEN *//
     case FAILED: {
       return { ...state, isLoading: false, error: action.payload };
     }
+
+    //* ACTIVE WHEN DATE SELECTED *//
     case SELECT_MONTHLY: {
       const selected_plan = state.plans.filter(
         (plan) => plan.date === action.payload
@@ -77,6 +85,7 @@ const monthlyReducer = (state = initialState, action) => {
           planDate[3] === selectedDate[3]
         );
       });
+      // SORTED BY DATE //
       unSelected_plan.sort(function (a, b) {
         const day1 = a.date.substring(9, 10);
         const day2 = b.date.substring(9, 10);
@@ -91,10 +100,13 @@ const monthlyReducer = (state = initialState, action) => {
         unSelected: unSelected_plan,
       };
     }
+
+    //* ACTIVE MONTHLY CREATED *//
     case CREATE_MONTHLY: {
       return { ...state, plans: [...state.plans, action.payload] };
     }
 
+    //* ACTIVE WHEN EeditBtn CLICKED *//
     case EDIT_MONTHLY: {
       const target = state.plans.find((plan) => plan.id === action.payload);
       const filteredPlans = state.plans.filter(
@@ -102,9 +114,13 @@ const monthlyReducer = (state = initialState, action) => {
       );
       return { ...state, isEdit: target, plans: filteredPlans };
     }
+
+    //* ACTIVE WHEN MONTHLY UPDATED *//
     case UPDATE_MONTHLY: {
       return { ...state, plans: [...state.plans, action.payload] };
     }
+
+    //* ACTIVE WHEN MONTHLY DELETED *//
     case DELETE_MONTHLY: {
       const filtered_selected = state.selected.filter(
         (plan) => plan.id !== action.payload.id
@@ -122,8 +138,29 @@ const monthlyReducer = (state = initialState, action) => {
         plans: filteredPlans,
       };
     }
+
+    //* ACTIVE WHEN MONTH & YEAR CHANGED *//
     case CHANGE_MONTHLY: {
-      return state;
+      const unSelected_plan = state.plans.filter((plan) => {
+        const monthYear = action.payload.split(" ");
+        const planDate = plan.date.split(" ");
+        return (
+          plan.date !== state.date &&
+          planDate[1] === monthYear[0].substring(0, 3) &&
+          planDate[3] === monthYear[1]
+        );
+      });
+      // SORTED BY DATE //
+      unSelected_plan.sort(function (a, b) {
+        const day1 = a.date.substring(9, 10);
+        const day2 = b.date.substring(9, 10);
+        return +day1 - +day2;
+      });
+      return {
+        ...state,
+        monthYear: action.payload,
+        unSelected: unSelected_plan,
+      };
     }
     default:
       return state;
