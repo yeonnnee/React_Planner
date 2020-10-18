939b2bfb2b12341219ec109eff09ad8ced04f655
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-18 21:34:03 +0900

    REVISE reason model

--- servers/models/reason.js
+++ servers/models/reason.js
@@ -4,11 +4,6 @@ const sequelize = require("./index");
 const Reason = sequelize.define(
   "reason",
   {
-    id: {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-    },
     deleteReason: {
       type: Sequelize.STRING(10),
       allowNull: false,
