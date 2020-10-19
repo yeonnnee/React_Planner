55b9895ee98a7865ac1db3b23a8d70be3e42eb83
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-19 15:30:12 +0900

    CREATE gateway error page for 504 error

---
+++ src/components/msg/GatewayError.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Text = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 20px;
+  margin-left: 20px;
+  font-family: "Fredericka the Great", cursive;
+`;
+const GatewayError = () => {
+  return (
+    <Container>
+      <Text>504 Gateway Time-out</Text>
+    </Container>
+  );
+};
+
+export default GatewayError;
