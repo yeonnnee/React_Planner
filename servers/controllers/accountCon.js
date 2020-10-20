d1817f482290614774a75f63f6e5def99494a830
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-20 23:32:41 +0900

    CREATE sequelize transaction

--- servers/controllers/accountCon.js
+++ servers/controllers/accountCon.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
 
+const sequelize = require("../models");
 const Content = require("../models/content");
 const Plan = require("../models/plan");
 const Task = require("../models/task");
@@ -67,35 +68,49 @@ exports.postDeleteAccount = async (req, res) => {
   try {
     const userEmail = req.session.user.email;
 
-    // 비밀번호 확인
-    const user = await User.findOne({ where: { email: userEmail } });
-    const password = req.body.password;
-    const compared_password = await bcrypt.compare(password, user.password);
-    // 비밀번호 일치시 데이터 삭제
-    if (compared_password) {
-      // task 리스트 삭제
-      await Task.destroy({ where: { userId: userEmail } });
-      // plan 과 content 삭제
-      const plans = await Plan.findAll({ where: { writer: userEmail } });
-      if (plans) {
-        for (const plan of plans) {
-          await Content.destroy({ where: { planId: plan.id } });
-          await Plan.destroy({ where: { id: plan.id } });
+    await sequelize.transaction(async (t) => {
+      // 비밀번호 확인
+      const user = await User.findOne(
+        { where: { email: userEmail } },
+        { transaction: t }
+      );
+      const password = req.body.password;
+      const compared_password = await bcrypt.compare(password, user.password);
+      // 비밀번호 일치시 데이터 삭제
+      if (compared_password) {
+        // task 리스트 삭제
+        await Task.destroy(
+          { where: { userId: userEmail } },
+          { transaction: t }
+        );
+        // plan 과 content 삭제
+        const plans = await Plan.findAll(
+          { where: { writer: userEmail } },
+          { transaction: t }
+        );
+        if (plans) {
+          for (const plan of plans) {
+            await Content.destroy(
+              { where: { planId: plan.id } },
+              { transaction: t }
+            );
+            await Plan.destroy({ where: { id: plan.id } }, { transaction: t });
+          }
         }
+        // 사유 저장
+        await Reason.create({
+          deleteReason: req.body.reason,
+          account: userEmail,
+        });
+        // 계정 삭제
+        await User.destroy({ where: { email: userEmail } }, { transaction: t });
+        // 세션 삭제
+        req.session.destroy();
+        res.status(201).json({ msg: "Account has been deleted successfully" });
+      } else {
+        res.status(400).json({ msg: "비밀번호가 일치하지 않습니다." });
       }
-      // 사유 저장
-      await Reason.create({
-        deleteReason: req.body.reason,
-        account: userEmail,
-      });
-      // 계정 삭제
-      await User.destroy({ where: { email: userEmail } });
-      // 세션 삭제
-      req.session.destroy();
-      res.status(201).json({ msg: "Account has been deleted successfully" });
-    } else {
-      res.status(400).json({ msg: "비밀번호가 일치하지 않습니다." });
-    }
+    });
   } catch (error) {
     throw new Error();
   }
--- servers/controllers/monthlyCon.js
+++ servers/controllers/monthlyCon.js
@@ -1,81 +1,119 @@
 const Plan = require("../models/plan");
 const Content = require("../models/content");
+const sequelize = require("../models");
 
 exports.getMonthly = async (req, res) => {
   try {
-    const user = req.session.user.email;
-    const plans = await Plan.findAll({ where: { writer: user } });
-    let monthly = [];
+    await sequelize.transaction(async (t) => {
+      const user = req.session.user.email;
+      const plans = await Plan.findAll(
+        { where: { writer: user } },
+        { transaction: t }
+      );
+      let monthly = [];
 
-    for (const plan of plans) {
-      const contents = await Content.findAll({ where: { planId: plan.id } });
-      const result = {
-        id: plan.id,
-        date: plan.date,
-        contents,
-      };
-      monthly.push(result);
-    }
+      for (const plan of plans) {
+        const contents = await Content.findAll(
+          { where: { planId: plan.id } },
+          { transaction: t }
+        );
+        const result = {
+          id: plan.id,
+          date: plan.date,
+          contents,
+        };
+        monthly.push(result);
+      }
 
-    res.status(200).json({ monthly: monthly });
+      res.status(200).json({ monthly: monthly });
+    });
   } catch (error) {
-    throw new Error();
+    if (error.name === "SequelizeDatabaseError") {
+      res.status(400).json({ msg: "Something went wrong" });
+    } else {
+      throw new Error();
+    }
   }
 };
 
 exports.postMonthly = async (req, res) => {
   try {
-    // Plan 생성 //
-    const user = req.session.user.email;
-    const planId = req.body.id;
-    const date = req.body.date;
-    await Plan.create({ id: planId, date: date, writer: user });
+    await sequelize.transaction(async (t) => {
+      // Plan 생성 //
+      const user = req.session.user.email;
+      const planId = req.body.id;
+      const date = req.body.date;
+      await Plan.create(
+        { id: planId, date: date, writer: user },
+        { transaction: t }
+      );
 
-    // Content 생성 //
-    const contents = req.body.contents;
+      // Content 생성 //
+      const contents = req.body.contents;
 
-    for (const content of contents) {
-      await Content.create({
-        id: content.id,
-        text: content.text,
-        planId: planId,
-      });
-    }
-
-    res.status(201).json({ msg: "Get data successfully" });
+      for (const content of contents) {
+        await Content.create(
+          {
+            id: content.id,
+            text: content.text,
+            planId: planId,
+          },
+          { transaction: t }
+        );
+      }
+      res.status(201).json({ msg: "Get data successfully" });
+    });
   } catch (error) {
-    throw new Error();
+    if (error.name === "SequelizeDatabaseError") {
+      res.status(400).json({ msg: "Something went wrong" });
+    } else {
+      throw new Error();
+    }
   }
 };
 
 exports.postEditMonthly = async (req, res) => {
   try {
-    const planId = req.body.id;
-    const updatedContents = req.body.contents;
-    await Content.destroy({ where: { planId: planId } });
+    await sequelize.transaction(async (t) => {
+      const planId = req.body.id;
+      const updatedContents = req.body.contents;
+      await Content.destroy({ where: { planId: planId } }, { transaction: t });
 
-    for (const content of updatedContents) {
-      await Content.create({
-        id: content.id,
-        text: content.text,
-        planId: planId,
-      });
-    }
-
-    res.status(201).json({ msg: "Patch Data Successfully" });
+      for (const content of updatedContents) {
+        await Content.create(
+          {
+            id: content.id,
+            text: content.text,
+            planId: planId,
+          },
+          { transaction: t }
+        );
+      }
+      res.status(201).json({ msg: "Patch Data Successfully" });
+    });
   } catch (error) {
-    throw new Error();
+    if (error.name === "SequelizeDatabaseError") {
+      res.status(400).json({ msg: "Something went wrong" });
+    } else {
+      throw new Error();
+    }
   }
 };
 
 exports.postDeleteMonthly = async (req, res) => {
   try {
-    const planId = req.body.id;
-    await Content.destroy({ where: { planId: planId } });
-    await Plan.destroy({ where: { id: planId } });
+    await sequelize.transaction(async (t) => {
+      const planId = req.body.id;
+      await Content.destroy({ where: { planId: planId } }, { transaction: t });
+      await Plan.destroy({ where: { id: planId } }, { transaction: t });
 
-    res.status(201).json({ msg: "Delete Data Successfully" });
-  } catch (erorr) {
-    throw new Error();
+      res.status(201).json({ msg: "Delete Data Successfully" });
+    });
+  } catch (error) {
+    if (error.name === "SequelizeDatabaseError") {
+      res.status(400).json({ msg: "Something went wrong" });
+    } else {
+      throw new Error();
+    }
   }
 };
