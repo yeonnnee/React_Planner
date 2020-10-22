7c258f5eba20512208bb02ceb58112d52100acd9
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-23 01:37:11 +0900

    CREATE removeMark function to remove mark when user delete plans and change onStatus function name to markingDate

--- src/screens/Monthly/Calendar.jsx
+++ src/screens/Monthly/Calendar.jsx
@@ -11,35 +11,42 @@ const MonthlyCalendar = styled(ReactCalendar)`
   padding: 10px;
 `;
 
+/* 
+  다음 버튼을 눌러도 getMonthYear는 새로운 값을 받아오지 못하는 문제를 해결하기 위해 
+  useEffect를 통해 init에 변화가 생길시 매 re render시켜 getMonthYear를 동작시켜야 했다. 
+  이를 위해 clicked라는 state를 만들어 init 이 동작할때마다, state 값을 올려주었고, 
+  state 값이 변경할때마다 init 은 동작하고, 
+  useEffect가 발동하여 getMonthYear가 새로운 값을 받아올 수 있게 되었다.
+  이때, clicked 함수를 사용하지 않고 바로 getMonthYear 와 init을 연결시켜버리면, 1달씩 밀려서 데이터가 저장되는 현상이 발생하게 된다.
+*/
+
 const Calendar = (calendarProps) => {
   const { select, change, state } = calendarProps;
   const [clicked, setClicked] = useState(0);
 
-  const onClickDay = (event) => {
-    const target = event.toString().substring(0, 15);
-    select(target);
-  };
+  // plans 삭제시 마크 지우는 함수
+  const removeMark = useCallback(async () => {
+    const monthYear = state.monthYear.split(" ");
+    const year = monthYear[1];
+    const month = monthYear[0];
 
-  const init = useCallback(() => {
-    const nextMonth = document.querySelector(
-      ".react-calendar__navigation__next-button"
-    );
-    const nextYear = document.querySelector(
-      ".react-calendar__navigation__next2-button"
-    );
-    const preYear = document.querySelector(
-      ".react-calendar__navigation__prev2-button"
-    );
-    const preMonth = document.querySelector(
-      ".react-calendar__navigation__prev-button"
-    );
-    nextMonth.onclick = () => setClicked(clicked + 1);
-    nextYear.onclick = () => setClicked(clicked + 1);
-    preMonth.onclick = () => setClicked(clicked + 1);
-    preYear.onclick = () => setClicked(clicked + 1);
-  }, [clicked]);
+    if (state.deleted) {
+      const deletedItem_Date = state.deleted.split(" ")[2];
+      if (deletedItem_Date.split("")[0] === "0") {
+        return deletedItem_Date.split("")[1];
+      }
+      const deletedAbbr = await document.querySelector(
+        `[aria-label= "${month} ${deletedItem_Date}, ${year}" ]`
+      );
 
-  const onStatus = useCallback(async () => {
+      deletedAbbr.style.padding = "";
+      deletedAbbr.style.borderRadius = "";
+      deletedAbbr.style.backgroundColor = "";
+    }
+  }, [state.deleted, state.monthYear]);
+
+  // plans 있는 날짜 표시하는 함수
+  const markingDate = useCallback(async () => {
     const monthYear = state.monthYear.split(" ");
     const year = monthYear[1];
     const month = monthYear[0];
@@ -48,6 +55,7 @@ const Calendar = (calendarProps) => {
       const planDate = plan.date.split(" ");
       return planDate[1] === month.substring(0, 3) && planDate[3] === year;
     });
+
     const targets = plans.map((plan) => {
       const number = plan.date.split(" ")[2];
       if (number.split("")[0] === "0") {
@@ -69,6 +77,33 @@ const Calendar = (calendarProps) => {
     }
   }, [state.monthYear, state.plans]);
 
+  // 날짜 선택 시 실행되는 함수
+  const onClickDay = (event) => {
+    const target = event.toString().substring(0, 15);
+    select(target);
+  };
+
+  // 네비게이션 버튼 클릭시 년도, 월 갱신시키는 함수
+  const init = useCallback(() => {
+    const nextMonth = document.querySelector(
+      ".react-calendar__navigation__next-button"
+    );
+    const nextYear = document.querySelector(
+      ".react-calendar__navigation__next2-button"
+    );
+    const preYear = document.querySelector(
+      ".react-calendar__navigation__prev2-button"
+    );
+    const preMonth = document.querySelector(
+      ".react-calendar__navigation__prev-button"
+    );
+    nextMonth.onclick = () => setClicked(clicked + 1);
+    nextYear.onclick = () => setClicked(clicked + 1);
+    preMonth.onclick = () => setClicked(clicked + 1);
+    preYear.onclick = () => setClicked(clicked + 1);
+  }, [clicked]);
+
+  // 년도, 월 받아오는 함수
   const getMonthYear = useCallback(async () => {
     const monthSection = await document.querySelector(
       ".react-calendar__navigation__label__labelText"
@@ -79,9 +114,10 @@ const Calendar = (calendarProps) => {
 
   useEffect(() => {
     getMonthYear();
-    onStatus();
+    markingDate();
+    removeMark();
     init();
-  }, [getMonthYear, init, onStatus]);
+  }, [getMonthYear, init, markingDate, removeMark]);
 
   return (
     <MonthlyCalendar
