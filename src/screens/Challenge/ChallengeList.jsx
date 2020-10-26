7db389344e4dc2afafdc5d3a14371ab1360b9a96
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-26 21:09:24 +0900

    REVISE challenge list style

--- src/screens/Challenge/ChallengeList.jsx
+++ src/screens/Challenge/ChallengeList.jsx
@@ -3,12 +3,18 @@ import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 import { ListItem, Column, Icon, Title, Status } from "./styles";
 
-const ChallengeList = () => {
+const ChallengeList = (challengeListProps) => {
+  const { title, status } = challengeListProps;
   return (
     <ListItem>
       <Column>
-        <Title>Read Books</Title>
-        <Status>In Progress / Day 1</Status>
+        <Title>{title}</Title>
+
+        {status === "ENROLLED" ? (
+          <Status>In Progress / Day1 </Status>
+        ) : (
+          <Status>FINISHED</Status>
+        )}
       </Column>
       <Column>
         <Icon icon={faChevronRight} />
