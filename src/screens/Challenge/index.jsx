1427d9b487d14fb7a82f8835c9d455606e6242a0
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-26 21:08:58 +0900

    FETCH challenge data

--- src/screens/Challenge/index.jsx
+++ src/screens/Challenge/index.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import { connect } from "react-redux";
+import { challengeApi } from "../../api";
+import {
+  FETCH_CHALLENGE,
+  FETCH_CHALLENGE_FAIELD,
+  FETCH_CHALLENGE_SUCCESS,
+} from "../../redux/types";
 
 import ChallengePresenter from "./ChallengePresenter";
 
-const Challenge = () => {
-  return <ChallengePresenter />;
+const Challenge = (challengeProps) => {
+  const {
+    state,
+    history,
+    fetchStart,
+    fetch_success,
+    fetch_failed,
+  } = challengeProps;
+
+  const fetchData = useCallback(async () => {
+    fetchStart();
+    try {
+      const res = await challengeApi.getChallenges();
+      console.log(res.data);
+      fetch_success(res.data.challenges);
+    } catch (error) {
+      const status = error.response.status;
+      fetch_failed();
+      if (status === 500) {
+        history.push("/500");
+      } else if (status === 504) {
+        history.push("/504");
+      }
+    }
+  }, [fetchStart, fetch_success, fetch_failed, history]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return <ChallengePresenter state={state} />;
 };
 
-export default Challenge;
+function mapStateToProps(state) {
+  return { state: state.challengeReducer };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchStart: () => {
+      dispatch({ type: FETCH_CHALLENGE });
+    },
+    fetch_success: (data) => {
+      dispatch({ type: FETCH_CHALLENGE_SUCCESS, payload: data });
+    },
+    fetch_failed: () => {
+      dispatch({ type: FETCH_CHALLENGE_FAIELD });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Challenge);
