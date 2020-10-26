28feea6cb36bdd802b8aa4e1103736f970f54675
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-26 21:08:40 +0900

    CREATE fetch challenge data action types and reducer

--- src/redux/reducers/challengeReducer.jsx
+++ src/redux/reducers/challengeReducer.jsx
@@ -1,12 +1,43 @@
+import {
+  FETCH_CHALLENGE,
+  FETCH_CHALLENGE_FAIELD,
+  FETCH_CHALLENGE_SUCCESS,
+} from "../types";
+
 const initialState = {
   isLoading: false,
-  proceeding: [],
+  enrolled: [],
   finished: [],
   challenges: [],
 };
 
 const challengeReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_CHALLENGE: {
+      return { ...state, isLoading: true };
+    }
+    case FETCH_CHALLENGE_SUCCESS: {
+      if (action.payload === []) {
+        return { ...state, isLoading: false };
+      } else {
+        const enrolledList = action.payload.filter(
+          (list) => list.status === "ENROLLED"
+        );
+        const finishedList = action.payload.filter(
+          (list) => list.status === "FINISHED"
+        );
+        return {
+          ...state,
+          isLoading: false,
+          porceeding: enrolledList,
+          finished: finishedList,
+          challenges: [...action.payload],
+        };
+      }
+    }
+    case FETCH_CHALLENGE_FAIELD: {
+      return { ...state, isLoading: false };
+    }
     default:
       return state;
   }
