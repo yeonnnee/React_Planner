d9e9094eb14b6aef535a35d40f61d4fe1bda3fc4
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-27 15:46:00 +0900

    REVISE recrod model

--- servers/models/record.js
+++ servers/models/record.js
@@ -1,20 +1,25 @@
 const Sequelize = require("sequelize");
 const sequelize = require("./index");
 
-const Record = sequelize.define("records", {
-  id: {
-    type: Sequelize.STRING(36),
-    allowNull: false,
-    primaryKey: true,
+const Record = sequelize.define(
+  "records",
+  {
+    day: {
+      type: Sequelize.STRING(6),
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.STRING(7),
+      allowNull: true,
+    },
+    date: {
+      type: Sequelize.STRING(16),
+      allowNull: false,
+    },
   },
-  status: {
-    type: Sequelize.STRING(7),
-    allowNull: true,
-  },
-  date: {
-    type: Sequelize.STRING(16),
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = Record;
