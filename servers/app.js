c7a90d7e35646e0d190bab26dfe830da194831d7
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-27 15:44:52 +0900

    CREATE enroll challenge router and set association btw challenge and record

--- servers/app.js
+++ servers/app.js
@@ -35,11 +35,13 @@ const monthlyRoutes = require("./routes/monthlyRoutes");
 const signUpRoutes = require("./routes/signUpRoutes");
 const authRoutes = require("./routes/authRoutes");
 const accountRoutes = require("./routes/accountRoutes");
+const challengeRoutes = require("./routes/challengeRoutes");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", signUpRoutes);
 app.use("/api/monthly", monthlyRoutes);
 app.use("/api/Account", accountRoutes);
+app.use("/api/challenge", challengeRoutes);
 app.use("/api", taskRoutes);
 
 // Association //
@@ -48,15 +50,24 @@ const Task = require("./models/task");
 const Plan = require("./models/plan");
 const Content = require("./models/content");
 const Reason = require("./models/reason");
+const Challenge = require("./models/challenge");
+const Record = require("./models/record");
 
 User.hasMany(Task, { foreignKey: "userId", sourceKey: "email" });
 User.hasOne(Reason, { foreignKey: "account", sourceKey: "email" });
 User.hasMany(Plan, { foreignKey: "writer", sourceKey: "email" });
+User.hasMany(Challenge, { foreignKey: "challenger", sourceKey: "email" });
 Reason.belongsTo(User);
 Task.belongsTo(User, { foreignKey: "userId", targetKey: "email" });
 Plan.belongsTo(User, { foreignKey: "writer", targetKey: "email" });
 Plan.hasMany(Content, { foreignKey: "planId", sourceKey: "id" });
 Content.belongsTo(Plan, { foreignKey: "planId", sourceKey: "id" });
+Challenge.hasMany(Record, { foreignKey: "challengeTitle", sourceKey: "title" });
+Challenge.belongsTo(User, { foreignKey: "challenger", targetKey: "email" });
+Record.belongsTo(Challenge, {
+  foreignKey: "challengeTitle",
+  targetKey: "title",
+});
 
 const driver = async () => {
   await sequelize.sync();
--- servers/routes/challengeRoutes.js
+++ servers/routes/challengeRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const challengeCon = require("../controllers/challengeCon");
 
 router.get("/", challengeCon.getChallenge);
+router.post("/enrolled", challengeCon.postChallenge);
 
 module.exports = router;
