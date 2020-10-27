4f05c277128bf1a29ca0f93982b2f05081a65525
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-27 15:49:35 +0900

    CREATE link to move page to challenge record and REVISE listItem style to desc ready status

--- src/screens/Challenge/ChallengeList.jsx
+++ src/screens/Challenge/ChallengeList.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
-import { ListItem, Column, Icon, Title, Status } from "./styles";
+import { ListItem, Column, Icon, Title, Status, SLink } from "./styles";
 
 const ChallengeList = (challengeListProps) => {
-  const { title, status } = challengeListProps;
+  const { title, status, record, id } = challengeListProps;
   return (
-    <ListItem>
-      <Column>
-        <Title>{title}</Title>
-
-        {status === "ENROLLED" ? (
-          <Status>In Progress / Day1 </Status>
-        ) : (
-          <Status>FINISHED</Status>
-        )}
-      </Column>
-      <Column>
-        <Icon icon={faChevronRight} />
-      </Column>
-    </ListItem>
+    <SLink to={`${id}`}>
+      <ListItem>
+        <Column>
+          <Title>{title}</Title>
+          {status === "ENROLLED" && record.length > 0 ? (
+            <Status>In Progress / `${record.day}` </Status>
+          ) : status === "ENROLLED" && record.length === 0 ? (
+            <Status>Ready / starts tomorrow</Status>
+          ) : (
+            <Status>FINISHED</Status>
+          )}
+        </Column>
+        <Column>
+          <Icon icon={faChevronRight} />
+        </Column>
+      </ListItem>
+    </SLink>
   );
 };
 
