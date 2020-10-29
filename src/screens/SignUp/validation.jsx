da36fc7e6697afc03675a093dd93af3543610835
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-29 22:15:49 +0900

    CHANGE setError to setValidationError

--- src/screens/SignUp/validation.jsx
+++ src/screens/SignUp/validation.jsx
@@ -1,35 +1,35 @@
-export const emailValidation = (val, setError) => {
+export const emailValidation = (val, setValidationError) => {
   if (!val.includes("@")) {
-    setError({ email: "유효하지 않은 이메일입니다." });
+    setValidationError({ email: "유효하지 않은 이메일입니다." });
   } else if (val.length > 80) {
-    setError({ email: "이메일은 80자 이내로 작성해주십시오." });
+    setValidationError({ email: "이메일은 80자 이내로 작성해주십시오." });
   } else {
-    setError({ email: "" });
+    setValidationError({ email: "" });
   }
 };
 
-export const passwordValidation = (val, setError) => {
+export const passwordValidation = (val, setValidationError) => {
   if (val.length < 8) {
-    setError({ password: "비밀번호는 8~16자로 작성해주십시오." });
+    setValidationError({ password: "비밀번호는 8~16자로 작성해주십시오." });
   } else if (val.length > 16) {
-    setError({ password: "비밀번호는 8~16자로 작성해주십시오." });
+    setValidationError({ password: "비밀번호는 8~16자로 작성해주십시오." });
   } else {
-    setError({ password: "" });
+    setValidationError({ password: "" });
   }
 };
 
-export const confirmPw_validation = (val, pw, setError) => {
+export const confirmPw_validation = (val, pw, setValidationError) => {
   if (val !== pw) {
-    setError({ confirmPw: "비밀번호와 일치하지 않습니다." });
+    setValidationError({ confirmPw: "비밀번호와 일치하지 않습니다." });
   } else {
-    setError({ confirmPw: "" });
+    setValidationError({ confirmPw: "" });
   }
 };
 
-export const nameValidation = (val, setError) => {
+export const nameValidation = (val, setValidationError) => {
   if (val.length > 20) {
-    setError({ name: "글자 수를 초과하였습니다." });
+    setValidationError({ name: "글자 수를 초과하였습니다." });
   } else {
-    setError({ name: "" });
+    setValidationError({ name: "" });
   }
 };
