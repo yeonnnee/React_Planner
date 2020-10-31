c05baf8bdd3d18b0edd1ffb744f794fdccc7ed0a
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-31 22:53:50 +0900

    Set up Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://13.125.110.43/",
+      target: "http://localhost:3001/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
