79d82ea4bfa9c0526f4425a934aff43b3390f9c3
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-01 00:48:49 +0900

    Set up Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "http://localhost:3001/",
+      target: "https://y-planner.herokuapp.com/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
