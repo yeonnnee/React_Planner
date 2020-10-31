3235b5caabb991037a900bd1570dcb4cfb519a56
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-10-31 22:36:17 +0900

    Set up Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,11 @@
 // React 에서 server로 api 요청 시 cors 관련 오류 방지
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const targetUrl = require("./environment");
 
 // /api로 시작되는 api는 target으로 설정된 서버 url로 호출
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: targetUrl,
+      target: "http://13.125.110.43/",
       changeOrigin: true,
       pathRewrite: {
         "^/api": "",
