bc06340b350387e74d2cef27fcd4738130e89f15
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-11-11 18:28:04 +0900

    set proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,19 +1,19 @@
 // React 에서 server로 api 요청 시 cors 관련 오류 방지
-// const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 // /api로 시작되는 api는 target으로 설정된 서버 url로 호출
-// module.exports = function (app) {
-//   app.use(
-//     createProxyMiddleware("/api", {
-//       target: "https://y-planner.herokuapp.com/",
-//       changeOrigin: true,
-//       pathRewrite: {
-//         "^/api": "",
-//         "/monthly/api": "",
-//         "/monthly/edit/api": "",
-//         "/challenge/api": "",
-//         "/account/api": "",
-//       },
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api", {
+      target: "https://y-planner.herokuapp.com/",
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api": "",
+        "/monthly/api": "",
+        "/monthly/edit/api": "",
+        "/challenge/api": "",
+        "/account/api": "",
+      },
+    })
+  );
+};
