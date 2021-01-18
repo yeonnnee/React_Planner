35207f19d907a8897fe95c9f4fc5193fd8910895
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-19 00:19:09 +0900

    [REVISE] change Frame and Container color

--- src/components/styles/Templates.jsx
+++ src/components/styles/Templates.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 800px;
   height: 880px;
-  background-color: #273c75;
+  background-color: #353535;
   box-shadow: 5px 5px 15px #615a58, 5px 5px 10px #615a58;
   display: flex;
   align-items: center;
@@ -13,7 +13,7 @@ export const Frame = styled.div`
   width: 100%;
   height: 150px;
   padding: 20px;
-  background-color: #ac6e6c;
+  background-color: #cbc5be;
 `;
 export const Main = styled.div`
   width: 100%;
