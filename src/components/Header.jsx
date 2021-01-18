770320fcd78bb25741e357ffa0c9252ed85c7c99
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-18 22:36:23 +0900

    [REVISE] welcomeMessage renamed to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,86 +1,53 @@
 import React from "react";
 import styled from "styled-components";
-import { connect } from "react-redux";
-import { Link, withRouter } from "react-router-dom";
+
+import Nav from "./Nav";
 
 const Container = styled.div`
   width: 100%;
-  position: relative;
-  top: -28px;
-  height: 30px;
+  padding: 20px 30px;
+  border: 2px solid grey;
   display: flex;
-  background-color: #c2b0ae;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-  box-shadow: 2px 0 5px #baa7a1, -2px 0 5px #baa7a1, inset -2px -2px 2px #baa7a1,
-    inset 2px -2px 5px #baa7a1;
-`;
-const List = styled.ul`
-  width: 100%;
-  height: 30px;
-  display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-around;
-`;
-const Menu = styled.li`
-  text-decoration: none;
-  font-weight: bold;
-  font-family: "Zilla Slab", serif;
-  color: ${(props) => (props.current ? "#4d3e3e" : "#835858")};
-  &:hover {
-    color: #4d3e3e;
+  border-bottom: 1px solid #382933;
+
+  @media only screen and (max-width: 640px) {
+    width: 90%;
+    padding: 0;
+    padding-bottom: 20px;
   }
 `;
-const SLink = styled(Link)`
-  text-decoration: none;
-`;
-
 const Title = styled.div`
-  height: 30px;
-  font-family: "Sacramento", cursive;
-  position: relative;
-  top: -50px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 30px;
+  font-family: "Cinzel Decorative", cursive;
+  font-size: 25px;
+
+  @media only screen and (max-width: 640px) {
+    font-size: 23px;
+  }
+`;
+const Describe = styled.div`
+  font-size: 13px;
+  margin-top: 20px;
+  line-height: 1.5;
+  text-align: center;
 `;
 
-const Header = withRouter((headerProps) => {
-  const { state, location } = headerProps;
-  const owner = state.user.split("@")[0];
+const Section = styled.div``;
 
+const Header = () => {
   return (
-    <>
-      {state.isAuthenticated ? (
-        <>
-          <Title>{owner}`s Planner</Title>
-          <Container>
-            <List>
-              <SLink to="/tasks">
-                <Menu current={location.pathname === "/tasks"}>Tasks</Menu>
-              </SLink>
-              <SLink to="/monthly">
-                <Menu current={location.pathname === "/monthly"}>Monthly</Menu>
-              </SLink>
-              <SLink to="/challenge">
-                <Menu current={location.pathname === "/challenge"}>
-                  Challenge
-                </Menu>
-              </SLink>
-              <SLink to="/account">
-                <Menu current={location.pathname === "/account"}>Account</Menu>
-              </SLink>
-            </List>
-          </Container>
-        </>
-      ) : null}
-    </>
+    <Container>
+      <Section>
+        <Title>Welcome to Y Planner</Title>
+        <Describe>
+          Manage your plan and record your day.
+          <br /> Dream, Plan, Do with our App.
+        </Describe>
+      </Section>
+      <Nav />
+    </Container>
   );
-});
+};
 
-function mapStateToProps(state) {
-  return { state: state.authReducer };
-}
-
-export default connect(mapStateToProps)(Header);
+export default Header;
