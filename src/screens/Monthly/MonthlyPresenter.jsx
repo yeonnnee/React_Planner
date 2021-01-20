edb0d335c9ef7c6aadc64226e01fbc359312a545
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-20 23:32:03 +0900

    [REVISE] monthly page UI

--- src/screens/Monthly/MonthlyPresenter.jsx
+++ src/screens/Monthly/MonthlyPresenter.jsx
@@ -3,96 +3,42 @@ import React from "react";
 import Calendar from "../../components/Calendar";
 import Header from "../../components/Header";
 import { Container, Frame, Main } from "../../components/styles/Templates";
+import MonthlyContent from "./MonthlyContent";
 
 import {
   Section,
   AddLink,
-  List,
-  Title,
   AddSection,
-  Wrapper,
-  SelectedMonthly,
-  Scroll,
-  UnSelectedMonthly,
-  Text,
-  SubTitle,
-  DetailLink,
+  Notice,
+  Column,
+  EditLink,
 } from "./styles";
 
 const MonthlyPresenter = (monthlyProps) => {
-  const { selected, unSelected, seeDetail } = monthlyProps;
-
+  const { selected } = monthlyProps;
+  console.log("monthlyPresenter", selected);
   return (
     <Container>
       <Frame>
         <Main>
           <Header />
-          <Calendar />
-
           <Section>
-            <SubTitle>Selected Plan</SubTitle>
-
-            {selected.length > 0 ? (
-              selected.map((plan, index) => {
-                const date = new Date(plan.date.substring(0, 15));
-                let month = date.getMonth() + 1;
-                const year = date.getFullYear();
-                let planDate = date.getDate();
-                if (planDate < 10) {
-                  planDate = "0" + planDate;
-                }
-                if (month < 10) {
-                  month = "0" + month;
-                }
-                return (
-                  <SelectedMonthly key={index}>
-                    <DetailLink to={`monthly/${plan.id}`}>
-                      <List id={plan.id} onClick={seeDetail}>
-                        <Title id={plan.id}>
-                          {month}-{planDate}-{year}
-                        </Title>
-                      </List>
-                    </DetailLink>
-                  </SelectedMonthly>
-                );
-              })
+            <Calendar />
+            <Column>
+              {selected[0].contents?.length > 0 ? (
+                <EditLink to={`/monthly/edit/${selected[0].id}`}>
+                  수정하기
+                </EditLink>
+              ) : null}
+            </Column>
+            {selected[0].contents?.length > 0 ? (
+              <MonthlyContent contents={selected[0].contents} />
             ) : (
               <AddSection>
+                <Notice>일정이 없습니다</Notice>
                 <AddLink to="/monthly/add">ADD</AddLink>
               </AddSection>
             )}
-            <SubTitle>Other Plans</SubTitle>
-            <Wrapper>
-              <Scroll>
-                <UnSelectedMonthly>
-                  {unSelected.length > 0 ? (
-                    unSelected.map((plan, index) => {
-                      const date = new Date(plan.date.substring(0, 15));
-                      let month = date.getMonth() + 1;
-                      const year = date.getFullYear();
-                      let planDate = date.getDate();
-                      if (planDate < 10) {
-                        planDate = "0" + planDate;
-                      }
-                      if (month < 10) {
-                        month = "0" + month;
-                      }
-                      return (
-                        <DetailLink to={`monthly/${plan.id}`} key={index}>
-                          <List id={plan.id} onClick={seeDetail}>
-                            <Title id={plan.id}>
-                              {month}-{planDate}-{year}
-                            </Title>
-                          </List>
-                        </DetailLink>
-                      );
-                    })
-                  ) : (
-                    <Text>Empty</Text>
-                  )}
-                </UnSelectedMonthly>
-              </Scroll>
-            </Wrapper>
           </Section>
         </Main>
       </Frame>
--- src/screens/Monthly/styles.jsx
+++ src/screens/Monthly/styles.jsx
@@ -1,17 +1,86 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+// monthly presenter
+
 export const Section = styled.div`
   width: 100%;
+  height: 600px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  @media only screen and (max-width: 640px) {
-    position: relative;
-    top: -20px;
-    margin: 0;
-    height: 150px;
+  border: 1px solid #dbd7d7;
+  margin-top: 20px;
+`;
+
+export const Column = styled.div`
+  font-family: "Rajdhani", sans-serif;
+  font-size: 16px;
+  padding: 3px 0;
+  opacity: 0.7;
+  width: 617px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  letter-spacing: 1.5px;
+  border-bottom: 1px solid #525151;
+`;
+
+// Monthly content
+
+export const SubTitle = styled.div`
+  width: 617px;
+  display: flex;
+  justify-content: space-between;
+  opacity: 0.7;
+  font-size: 14px;
+  margin-top: 3px;
+`;
+export const Text = styled.div`
+  font-family: "Rajdhani", sans-serif;
+  font-size: 15px;
+`;
+
+export const EditLink = styled(Link)`
+  text-decoration: none;
+  font-size: 12px;
+  color: #9e8181;
+
+  &:hover {
+    color: #611e1e;
   }
+`;
+
+export const Wrapper = styled.div`
+  width: 618px;
+  overflow: hidden;
+`;
+
+export const Scroll = styled.div`
+  width: 635px;
+`;
+
+export const List = styled.ul`
+  width: 617px;
+  height: 230px;
+  overflow: auto;
+  padding-top: 10px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: 20px;
+  gap: 5px;
+`;
+export const ListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+`;
+
+// Add Section
+
+export const AddSection = styled.div`
+  display: grid;
+  place-items: center;
+  transform: translateY(80px);
 `;
 
 export const AddLink = styled(Link)`
@@ -19,126 +88,16 @@ export const AddLink = styled(Link)`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #30475e;
-  color: #feca57;
+  background-color: #525151;
+  color: #f3f1f1;
   width: 80px;
   height: 30px;
-  border: 1px solid #30475e;
-  border-radius: 3px;
-  font-family: "Rajdhani", sans-serif;
-`;
-
-export const DetailLink = styled(Link)`
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const AddSection = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  border: 1px solid #353535;
   margin-top: 20px;
-  height: 50px;
-`;
-
-export const List = styled.li`
-  width: 100%;
-  height: 70px;
-  margin: 5px 0;
-  border: 1px solid #30475e;
-  outline: none;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-family: "Rajdhani", sans-serif;
-  font-size: 18px;
-  background-color: #4b5d67;
-  color: #c2b0ad;
-  z-index: 1;
-  &:hover {
-    background-color: #374851;
-  }
-  &:focus {
-    background-color: #374851;
-  }
-`;
-export const Title = styled.div``;
-export const Wrapper = styled.div`
-  width: 460px;
-  height: 250px;
-  overflow: hidden;
-
-  @media only screen and (max-width: 640px) {
-    display: none;
-  }
-  @media only screen and (max-width: 700px) {
-    width: 350px;
-    height: 100px;
-  }
-`;
-export const SelectedMonthly = styled.ul`
-  width: 90%;
-  height: 100px;
-  display: grid;
-  z-index: 1;
-  padding: 10px;
-  /* &:focus-within {
-    height: 250px;
-  }
-  &:focus-within ~ ${Wrapper} {
-    display: none;
-  } */
-`;
-
-export const Scroll = styled.div`
-  width: 480px;
-  height: 240px;
-  overflow: auto;
-  @media only screen and (max-width: 640px) {
-    width: 350px;
-    height: 100px;
-  }
-  @media only screen and (max-width: 700px) {
-    width: 370px;
-    height: 100px;
-  }
-`;
-export const UnSelectedMonthly = styled.ul`
-  height: 260px;
-  padding: 20px;
-  @media only screen and (max-width: 640px) {
-    width: 330px;
-    height: 130px;
-    position: relative;
-    top: -15px;
-  }
-  @media only screen and (max-width: 700px) {
-    width: 350px;
-    height: 100px;
-  }
-`;
-export const Text = styled.div`
-  font-family: "Rajdhani", sans-serif;
-  display: flex;
-  justify-content: center;
-  margin-top: 30px;
-`;
-
-export const SubTitle = styled.div`
-  font-family: "Rajdhani", sans-serif;
   font-size: 13px;
+`;
+
+export const Notice = styled.div`
   opacity: 0.7;
-  width: 85%;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  letter-spacing: 1.5px;
-  border-bottom: 1px solid #30475e;
-  @media only screen and (max-width: 640px) {
-    &:nth-child(3) {
-      display: none;
-    }
-  }
+  font-size: 14px;
 `;
