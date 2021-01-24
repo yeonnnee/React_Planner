5e8c83ecf087921f2c00cc870e40b26da87a6710
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-25 03:27:39 +0900

    [REVISE] Find PW page design

--- src/screens/FindPassword/FindPasswordPresenter.jsx
+++ src/screens/FindPassword/FindPasswordPresenter.jsx
@@ -2,27 +2,38 @@ import React from "react";
 
 import Error from "../../components/msg/ErrorMessage";
 import Loader from "../../components/Loader";
-import { Container, Title, Text, Form, Input, Section, Button } from "./styles";
+import Header from "../../components/Header";
+import { Container, Frame, Main } from "../../components/styles/Templates";
+import { Section } from "../Account/Verification/styles";
+import { Column } from "../SignUp/styles";
+import { Title, Text, Form, Input, Button } from "./styles";
 
 const FindPasswordPresenter = (findPwProps) => {
   const { state, user, onChange, onClick, onCancel } = findPwProps;
   return (
     <Container>
-      <Title>비밀번호 찾기</Title>
-      <Text>비밀번호 재설정을 위해 이메일을 입력해 주세요</Text>
-      {state.isLoading ? <Loader /> : null}
-      {user.error ? <Error error={user.error} /> : null}
-      <Form>
-        <Input
-          type="email"
-          placeholder="이메일을 입력하세요"
-          onChange={onChange}
-        />
-      </Form>
-      <Section>
-        <Button onClick={onClick}>확인</Button>
-        <Button onClick={onCancel}>취소</Button>
-      </Section>
+      <Frame>
+        <Main>
+          <Header />
+          <Section>
+            <Title>비밀번호 찾기</Title>
+            <Text>비밀번호 재설정을 위해 이메일을 입력해 주세요</Text>
+            {state.isLoading ? <Loader /> : null}
+            {user.error ? <Error error={user.error} /> : null}
+            <Form>
+              <Input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                onChange={onChange}
+              />
+            </Form>
+            <Column>
+              <Button onClick={onClick}>확인</Button>
+              <Button onClick={onCancel}>취소</Button>
+            </Column>
+          </Section>
+        </Main>
+      </Frame>
     </Container>
   );
 };
