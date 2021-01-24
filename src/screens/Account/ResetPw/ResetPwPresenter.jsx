6ce997181022ffb09c43c28e0304b9bcfe95a223
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-24 23:46:03 +0900

    [REVISE] resetPw page styles

--- src/screens/Account/ResetPw/ResetPwPresenter.jsx
+++ src/screens/Account/ResetPw/ResetPwPresenter.jsx
@@ -2,41 +2,55 @@ import React from "react";
 
 import Input from "../../SignUp/SignUpInput";
 import Loader from "../../../components/Loader";
-import { Container, Text, Section, Error, Button } from "./styles";
+import Header from "../../../components/Header";
+import { Container, Frame, Main } from "../../../components/styles/Templates";
+import { Section, Text } from "../Verification/styles";
+import { Error, Button, Notice } from "./styles";
 
 const ResetPwPresenter = (resetPwProps) => {
   const { onChange, state, onClick } = resetPwProps;
   return (
     <Container>
-      <Text>비밀번호 변경</Text>
-      {state.isLoading ? (
-        <Section>
-          <Loader />
-        </Section>
-      ) : null}
-      <Input
-        type="password"
-        placeholder="Password"
-        id="새 비밀번호"
-        onChange={onChange}
-        error={state.validation.updatedPassword ? true : false}
-      />
-      {state.validation.updatedPassword ? (
-        <Error>{state.validation.updatedPassword}</Error>
-      ) : null}
+      <Frame>
+        <Main>
+          <Header />
+          <Section>
+            <Text>비밀번호 변경</Text>
 
-      <Input
-        type="password"
-        placeholder="Confirm Password"
-        id="비밀번호 재입력"
-        onChange={onChange}
-        error={state.validation.confirmPassword ? true : false}
-      />
-      {state.validation.confirmPassword ? (
-        <Error>{state.validation.confirmPassword}</Error>
-      ) : null}
+            {state.isLoading ? (
+              <Loader />
+            ) : (
+              <Notice>
+                *비밀번호는 영대소문자, 특수문자, 숫자를 포함한 8~16자로 작성해
+                주십시오
+              </Notice>
+            )}
+            <Input
+              type="password"
+              placeholder="Password"
+              id="새 비밀번호"
+              onChange={onChange}
+              error={state.validation.updatedPassword ? true : false}
+            />
+            {state.validation.updatedPassword ? (
+              <Error>{state.validation.updatedPassword}</Error>
+            ) : null}
 
-      <Button onClick={onClick}>비밀번호 변경</Button>
+            <Input
+              type="password"
+              placeholder="Confirm Password"
+              id="비밀번호 재입력"
+              onChange={onChange}
+              error={state.validation.confirmPassword ? true : false}
+            />
+            {state.validation.confirmPassword ? (
+              <Error>{state.validation.confirmPassword}</Error>
+            ) : null}
+
+            <Button onClick={onClick}>비밀번호 변경</Button>
+          </Section>
+        </Main>
+      </Frame>
     </Container>
   );
 };
--- src/screens/Account/ResetPw/styles.jsx
+++ src/screens/Account/ResetPw/styles.jsx
@@ -1,39 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  margin-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  @media only screen and (max-width: 640px) {
-    margin-bottom: 100px;
-  }
-`;
-export const Text = styled.div`
-  font-size: 18px;
-  margin-bottom: 30px;
-`;
-export const Section = styled.div`
-  margin-bottom: 20px;
-`;
-
 export const Error = styled.div`
-  position: relative;
-  top: -20px;
-  left: 35px;
-  width: 300px;
+  width: 100%;
   height: 20px;
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
   font-size: 12px;
   color: red;
-  line-height: 1.5;
-  @media only screen and (max-width: 640px) {
-    top: -17px;
-  }
+  transform: translateX(137px) translateY(-15px);
 `;
+
+export const Notice = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+  font-size: 12px;
+  margin-bottom: 30px;
+`;
+
 export const Button = styled.button`
   outline: none;
   border: none;
@@ -44,6 +29,7 @@ export const Button = styled.button`
   height: 30px;
   margin-top: 20px;
   background: linear-gradient(to left top, #b71540, #eb5757);
+
   &:active {
     background: linear-gradient(to right bottom, #b71540, #eb5757);
   }
