83c7888d912b2ad1e1f1c59ea480a3aa664e38a5
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-25 03:28:33 +0900

    [REVISE] find pw page styles

--- src/screens/FindPassword/styles.jsx
+++ src/screens/FindPassword/styles.jsx
@@ -1,18 +1,5 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  margin-top: 45px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  @media only screen and (max-width: 640px) {
-    height: 95%;
-    margin-top: 0;
-    justify-content: center;
-    position: relative;
-    top: -50px;
-  }
-`;
 export const Title = styled.div`
   font-size: 20px;
 `;
@@ -28,13 +15,14 @@ export const Input = styled.input`
   outline: none;
   width: 350px;
   padding: 10px 20px;
-  @media only screen and (max-width: 640px) {
-    width: 250px;
+  margin-bottom: 40px;
+  border: 1px solid #dfdede;
+
+  &:focus {
+    border: 1px solid #30475e;
   }
 `;
-export const Section = styled.div`
-  margin-top: 30px;
-`;
+
 export const Button = styled.button`
   width: 100px;
   height: 30px;
