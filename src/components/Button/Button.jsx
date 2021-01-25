464b921178dc08e3962b0adb15677420a9c5589f
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-01-25 17:30:48 +0900

    [REVISE] create styles file

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,34 +1,5 @@
 import React from "react";
-import styled from "styled-components";
-
-export const Btn = styled.button`
-  width: 150px;
-  height: 40px;
-  margin-right: 10px;
-  background-color: #30475e;
-  color: white;
-  border: 1px solid white;
-  opacity: 0.8;
-  outline: none;
-
-  &:nth-child(2) {
-    background-color: transparent;
-    color: #30475e;
-    border: 1px solid #30475e;
-    &:hover {
-      background-color: transparent;
-      color: #30475e;
-      border: 1px solid #30475e;
-      box-shadow: 2px 2px 5px #3e5368, 0 0 2px #30475e;
-    }
-  }
-
-  &:hover {
-    background-color: #30475e;
-    border: 1px solid #30475e;
-    box-shadow: 2px 2px 5px #3e5368, 0 0 2px #30475e;
-  }
-`;
+import { Btn } from "./styles";
 
 const Button = (buttonProps) => {
   const { onClick, text } = buttonProps;
